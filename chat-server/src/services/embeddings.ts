852084e93e4d58eb8150e62ce6174cb3776fcf4e
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2023-06-12 09:45:08 -0400

    Add embedding service stubbed out

--- chat-server/src/services/embeddings.ts
+++ chat-server/src/services/embeddings.ts
@@ -1,6 +1,38 @@
-// TODO: define:
-// following same pattern as llm service here
-// - generic embeddings service
-// - abstract embeddings provider
-// - openai embeddings provider which implements embeddings provider
-// - export default embeddings which uses instance of openai embeddings provider
+import { Configuration, OpenAIApi } from 'openai';
+
+class EmbeddingService {
+  private embeddingProvider: EmbeddingProvider;
+  constructor(embeddingProvider: EmbeddingProvider) {
+    this.embeddingProvider = embeddingProvider;
+  }
+
+  async createEmbedding(text: string) {
+    return await this.embeddingProvider.createEmbedding(text);
+  }
+}
+
+abstract class EmbeddingProvider {
+  abstract createEmbedding(text: string): Promise<number[]>;
+}
+
+class OpenAIEmbeddingProvider extends EmbeddingProvider {
+  private openaiClient: OpenAIApi;
+
+  constructor(endpoint: string, apiKey: string) {
+    super();
+    const configuration = new Configuration({
+      basePath: endpoint, // TODO: validate that this correct..never used endpoint besides their default before
+      apiKey: apiKey,
+    });
+    this.openaiClient = new OpenAIApi(configuration);
+  }
+
+  async createEmbedding(text: string) {
+    // TODO: do stuff with the openaiClient
+    return [1, 2, 3];
+  }
+}
+
+const openAIProvider = new OpenAIEmbeddingProvider(process.env.OPENAI_ENDPOINT!, process.env.OPENAI_API_KEY!);
+const embeddings = new EmbeddingService(openAIProvider);
+export { embeddings };

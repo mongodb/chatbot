f6979f5ce701b33224854cad2b905fb7c5d29977
Chris Bush <chris.bush@mongodb.com>
2023-06-13 10:44:49 -0400

    Fix .env.example and tsconfig

--- chat-server/.eslintrc.js
+++ chat-server/.eslintrc.js
@@ -1,15 +1,15 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  extends: ['plugin:@typescript-eslint/recommended', 'prettier'],
-  ignorePatterns: ['dist/', 'node_modules/'],
-  plugins: ['prettier'],
+  parser: "@typescript-eslint/parser",
+  extends: ["plugin:@typescript-eslint/recommended", "prettier"],
+  ignorePatterns: ["dist/", "node_modules/"],
+  plugins: ["prettier"],
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: "module",
   },
   rules: {
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-non-null-assertion': 'off',
-    'prettier/prettier': 'error',
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "prettier/prettier": "error",
   },
 };

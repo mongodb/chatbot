7be6f666071fbccf47a6b1e2f0e549201a78e743
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2023-06-27 11:20:24 -0400

    set up test env in drone pipeline/atlas

--- chat-server/seed-content/index.ts
+++ chat-server/seed-content/index.ts
@@ -1,21 +1,39 @@
 import fs from "fs";
 import dotenv from "dotenv";
-import { Content } from "chat-core";
+import {
+  Content,
+  EmbeddingService,
+  OpenAiEmbeddingProvider,
+  OpenAiEmbeddingsClient,
+} from "chat-core";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import { Document } from "langchain/dist/document";
 import GPT3Tokenizer from "gpt3-tokenizer";
 import { MongoClient, ObjectId } from "mongodb";
-// TODO:(DOCSP-30994) refactor the chat-core to not export the instantiated object. weird stuff happens when using staging env
-// import { embeddings } from "chat-core";
 
 const [_, __, envFile] = process.argv;
 dotenv.config({ path: envFile });
 console.log("envFile", envFile);
 
-const { MONGODB_CONNECTION_URI, MONGODB_DATABASE_NAME } = process.env;
+const {
+  MONGODB_CONNECTION_URI,
+  MONGODB_DATABASE_NAME,
+  OPENAI_ENDPOINT,
+  OPENAI_API_KEY,
+  OPENAI_EMBEDDING_DEPLOYMENT,
+  OPENAI_EMBEDDING_MODEL_VERSION,
+} = process.env;
 console.log("MONGODB_CONNECTION_URI", MONGODB_CONNECTION_URI!);
 console.log("MONGODB_DATABASE_NAME", MONGODB_DATABASE_NAME!);
 const mongodb = new MongoClient(MONGODB_CONNECTION_URI!);
+const openaiClient = new OpenAiEmbeddingsClient(
+  OPENAI_ENDPOINT!,
+  OPENAI_EMBEDDING_DEPLOYMENT!,
+  OPENAI_API_KEY!,
+  OPENAI_EMBEDDING_MODEL_VERSION!
+);
+const openAiEmbeddingProvider = new OpenAiEmbeddingProvider(openaiClient);
+const embeddings = new EmbeddingService(openAiEmbeddingProvider);
 
 /**
  * Note that not capturing all fields, just ones used in the content mapping.

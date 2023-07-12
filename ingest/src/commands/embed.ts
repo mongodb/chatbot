92d22c3c9113167d10d4ad8ba19438b9428c9f98
Chris Bush <chris.bush@mongodb.com>
2023-07-12 17:43:34 -0400

    Implement 'all' command

--- ingest/src/commands/embed.ts
+++ ingest/src/commands/embed.ts
@@ -3,13 +3,15 @@ import {
   makeDatabaseConnection,
   assertEnvVars,
   makeOpenAiEmbedFunc,
+  EmbeddedContentStore,
+  PageStore,
 } from "chat-core";
 import { updateEmbeddedContent } from "../updateEmbeddedContent";
 import { INGEST_ENV_VARS } from "../IngestEnvVars";
 
 type EmbeddedContentCommandArgs = {
   since: string;
-  source?: string;
+  source?: string | string[];
 };
 
 const commandModule: CommandModule<unknown, EmbeddedContentCommandArgs> = {
@@ -18,45 +20,20 @@ const commandModule: CommandModule<unknown, EmbeddedContentCommandArgs> = {
     return args.string("since").string("source").demandOption("since");
   },
   async handler({ since, source }) {
-    const {
-      MONGODB_CONNECTION_URI,
-      MONGODB_DATABASE_NAME,
-      OPENAI_ENDPOINT,
-      OPENAI_API_KEY,
-      OPENAI_EMBEDDING_MODEL_VERSION,
-      OPENAI_EMBEDDING_DEPLOYMENT,
-    } = assertEnvVars(INGEST_ENV_VARS);
-
-    const embed = makeOpenAiEmbedFunc({
-      baseUrl: OPENAI_ENDPOINT,
-      apiKey: OPENAI_API_KEY,
-      apiVersion: OPENAI_EMBEDDING_MODEL_VERSION,
-      deployment: OPENAI_EMBEDDING_DEPLOYMENT,
-      backoffOptions: {
-        numOfAttempts: 25,
-        startingDelay: 1000,
-      },
-    });
+    const { MONGODB_CONNECTION_URI, MONGODB_DATABASE_NAME } =
+      assertEnvVars(INGEST_ENV_VARS);
 
     const store = await makeDatabaseConnection({
       connectionUri: MONGODB_CONNECTION_URI,
       databaseName: MONGODB_DATABASE_NAME,
     });
 
-    const sourceNames =
-      source === undefined
-        ? undefined
-        : Array.isArray(source)
-        ? source
-        : [source];
-
     try {
-      await updateEmbeddedContent({
-        since: new Date(since),
-        sourceNames,
+      await doEmbedCommand({
         pageStore: store,
         embeddedContentStore: store,
-        embed,
+        since: new Date(since),
+        source,
       });
     } finally {
       await store.close();
@@ -66,3 +43,48 @@ const commandModule: CommandModule<unknown, EmbeddedContentCommandArgs> = {
 };
 
 export default commandModule;
+
+export const doEmbedCommand = async ({
+  pageStore,
+  embeddedContentStore,
+  since,
+  source,
+}: {
+  since: Date;
+  pageStore: PageStore;
+  embeddedContentStore: EmbeddedContentStore;
+  source?: string | string[];
+}) => {
+  const {
+    OPENAI_ENDPOINT,
+    OPENAI_API_KEY,
+    OPENAI_EMBEDDING_MODEL_VERSION,
+    OPENAI_EMBEDDING_DEPLOYMENT,
+  } = assertEnvVars(INGEST_ENV_VARS);
+
+  const embed = makeOpenAiEmbedFunc({
+    baseUrl: OPENAI_ENDPOINT,
+    apiKey: OPENAI_API_KEY,
+    apiVersion: OPENAI_EMBEDDING_MODEL_VERSION,
+    deployment: OPENAI_EMBEDDING_DEPLOYMENT,
+    backoffOptions: {
+      numOfAttempts: 25,
+      startingDelay: 1000,
+    },
+  });
+
+  const sourceNames =
+    source === undefined
+      ? undefined
+      : Array.isArray(source)
+      ? source
+      : [source];
+
+  await updateEmbeddedContent({
+    since,
+    sourceNames,
+    pageStore,
+    embeddedContentStore,
+    embed,
+  });
+};

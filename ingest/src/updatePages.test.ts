d296d039bac2cd5a89ce1b7f6d1116ee8ffdb16e
Chris Bush <chris.bush@mongodb.com>
2023-07-25 11:32:35 -0400

    Add tests for data deletion
    - Verifies that old data will be deleted if the page is marked deleted

--- ingest/src/updatePages.test.ts
+++ ingest/src/updatePages.test.ts
@@ -38,4 +38,39 @@ describe("persistPages", () => {
       action: "created",
     });
   });
+
+  it("deletes pages when the base url changed", async () => {
+    const store = makeMockPageStore();
+
+    await persistPages({
+      pages: [{ ...examplePage }],
+      store,
+      sourceName: "test",
+    });
+
+    let pages = await store.loadPages();
+    expect(pages.length).toBe(1);
+    expect(pages[0]).toMatchObject<Partial<PersistedPage>>({
+      ...examplePage,
+      action: "created",
+    });
+    const v2Url = `${examplePage.url}/v2`;
+    await persistPages({
+      pages: [{ ...examplePage, url: v2Url }],
+      store,
+      sourceName: "test",
+    });
+
+    pages = await store.loadPages();
+    expect(pages.length).toBe(2);
+    expect(pages[0]).toMatchObject<Partial<PersistedPage>>({
+      ...examplePage,
+      action: "deleted",
+    });
+    expect(pages[1]).toMatchObject<Partial<PersistedPage>>({
+      ...examplePage,
+      url: v2Url,
+      action: "created",
+    });
+  });
 });

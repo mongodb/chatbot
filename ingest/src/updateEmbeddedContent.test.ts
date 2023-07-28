9b1417a30c5eb95db18a149cb08c3b99acb3c68c
Chris Bush <chris.bush@mongodb.com>
2023-07-28 15:49:45 -0400

    Add tests for data deletion (#48)
    - Verifies that old data will be deleted if the page is marked deleted

---
+++ ingest/src/updateEmbeddedContent.test.ts
@@ -0,0 +1,85 @@
+import { EmbeddedContentStore, EmbeddedContent, Page } from "chat-core";
+import { updateEmbeddedContent } from "./updateEmbeddedContent";
+import { makeMockPageStore } from "./updatePages.test";
+import { persistPages } from "./updatePages";
+
+export const makeMockEmbeddedContentStore = (): EmbeddedContentStore => {
+  const content: Map<string /* page url */, EmbeddedContent[]> = new Map();
+  return {
+    async deleteEmbeddedContent({ page }) {
+      content.set(page.url, []);
+    },
+    async findNearestNeighbors() {
+      return [];
+    },
+    async loadEmbeddedContent({ page }) {
+      return content.get(page.url) ?? [];
+    },
+    async updateEmbeddedContent({ embeddedContent, page }) {
+      content.set(page.url, [...embeddedContent]);
+    },
+  };
+};
+
+const examplePage: Page = {
+  body: "this is a test page",
+  format: "md",
+  sourceName: "test",
+  tags: [],
+  url: "https://example.com/test",
+};
+
+const embed = async () => {
+  return { embedding: [1, 2, 3] };
+};
+
+describe("updateEmbeddedContent", () => {
+  it("deletes embedded content for deleted page", async () => {
+    const pageStore = makeMockPageStore();
+
+    await persistPages({
+      pages: [{ ...examplePage }],
+      store: pageStore,
+      sourceName: "test",
+    });
+    let pages = await pageStore.loadPages();
+    expect(pages).toHaveLength(1);
+
+    const embeddedContentStore = makeMockEmbeddedContentStore();
+
+    const since = new Date("2000-01-01");
+
+    await updateEmbeddedContent({
+      embed,
+      embeddedContentStore,
+      pageStore,
+      since,
+    });
+
+    let embeddedContent = await embeddedContentStore.loadEmbeddedContent({
+      page: examplePage,
+    });
+    expect(embeddedContent).toHaveLength(1);
+
+    await persistPages({
+      pages: [],
+      store: pageStore,
+      sourceName: "test",
+    });
+    pages = await pageStore.loadPages();
+    expect(pages).toHaveLength(1);
+    expect(pages[0].action).toBe("deleted");
+
+    await updateEmbeddedContent({
+      embed,
+      embeddedContentStore,
+      pageStore,
+      since,
+    });
+    embeddedContent = await embeddedContentStore.loadEmbeddedContent({
+      page: examplePage,
+    });
+    // Embedded content for page was deleted
+    expect(embeddedContent).toHaveLength(0);
+  });
+});

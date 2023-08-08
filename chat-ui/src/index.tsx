56d313140557491c04a4a5283274625c567a063d
Nick Larew <nick.larew@mongodb.com>
2023-08-08 17:08:18 -0400

    Ignore irrelevant lint error in index file

--- chat-ui/src/index.tsx
+++ chat-ui/src/index.tsx
@@ -1,6 +1,7 @@
 export { Chatbot } from "./Chatbot";
 export { SuggestedPrompts, type SuggestedPrompt } from "./SuggestedPrompts.tsx";
 export {
+  // eslint-disable-next-line react-refresh/only-export-components
   useConversation,
   type ConversationState,
   type Conversation,

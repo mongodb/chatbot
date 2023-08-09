539d40393cff73da4a166056cfbfc689e6e3d05c
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2023-08-09 15:29:25 -0400

    (DOCSP-31925): Support tables in parsed snooty MD  (#67)
    * handle tables as HTML tables
    
    * start tests
    
    * nested tables
    
    * cleanup
    
    * Fix list parsing within tables
    
    Use a state enum rather than bool for parent table
    
    * Update ingest/src/snootyAstToMd.ts
    
    Co-authored-by: Nick Larew <nick.larew@10gen.com>
    
    * Implement multiple header rows (??)
    
    ---------
    
    Co-authored-by: Chris Bush <chris.bush@mongodb.com>
    Co-authored-by: Chris Bush <chris.bush@10gen.com>
    Co-authored-by: Nick Larew <nick.larew@10gen.com>

--- ingest/src/snootyAstToMd.test.ts
+++ ingest/src/snootyAstToMd.test.ts
@@ -1,6 +1,7 @@
 import Path from "path";
 import { readFileSync } from "fs";
 import { snootyAstToMd, getTitleFromSnootyAst } from "./snootyAstToMd";
+import { SnootyNode } from "./SnootyDataSource";
 
 describe("snootyAstToMd", () => {
   const samplePage = JSON.parse(
@@ -96,6 +97,275 @@ describe("snootyAstToMd", () => {
       expect(result).not.toContain("```undefined\n");
     });
   });
+
+  it("renders HTML tables", () => {
+    const samplePage = JSON.parse(
+      readFileSync(
+        Path.resolve(__dirname, "./test_data/sampleListTable.json"),
+        {
+          encoding: "utf-8",
+        }
+      )
+    );
+    const result = snootyAstToMd(samplePage.data.ast, {
+      baseUrl: "/",
+    });
+    const expected = `<table>
+<tr>
+<th>
+Action
+
+</th>
+<th>
+Description
+
+</th>
+</tr>
+<tr>
+<td>
+"replace"
+
+</td>
+<td>
+Replace the existing document in the output collection with the matching results document.
+
+When performing a replace, the replacement document cannot result in a modification of the \`_id\` value or, if the output collection is sharded, the shard key value. Otherwise, the operation generates an error.
+
+To avoid this error, if the on field does not include the \`_id\` field, remove the \`_id\` field in the aggregation results to avoid the error, such as with a preceding \`$unset\` stage, and so on.
+
+</td>
+</tr>
+<tr>
+<td>
+"keepExisting"
+
+</td>
+<td>
+Keep the existing document in the output collection.
+
+</td>
+</tr>
+<tr>
+<td>
+"merge" (Default)
+
+</td>
+<td>
+Merge the matching documents (similar to the \`$mergeObjects\` operator).
+
+- If the results document contains fields not in the existing document, add these new fields to the existing document.
+
+- If the results document contains fields in the existing document, replace the existing field values with those from the results document.
+
+For example, if the output collection has the document:
+
+\`{ _id: 1, a: 1, b: 1 }\`
+
+And the aggregation results has the document:
+
+\`{ _id: 1, b: 5, z: 1 }\`
+
+Then, the merged document is:
+
+\`{ _id: 1, a: 1, b: 5, z: 1 }\`
+
+When performing a merge, the merged document cannot result in a modification of the \`_id\` value or, if the output collection is sharded, the shard key value. Otherwise, the operation generates an error.
+
+To avoid this error, if the on field does not include the \`_id\` field, remove the \`_id\` field in the aggregation results to avoid the error, such as with a preceding \`$unset\` stage, and so on.
+
+</td>
+</tr>
+<tr>
+<td>
+"fail"
+
+</td>
+<td>
+Stop and fail the aggregation operation. Any changes to the output collection from previous documents are not reverted.
+
+</td>
+</tr>
+</table>`;
+    expect(result).toBe(expected);
+    const openingTagCount = result.split("<td>").length - 1;
+    const closingTagCount = result.split("</td>").length - 1;
+    expect(openingTagCount).toBe(8);
+    expect(openingTagCount).toBe(closingTagCount);
+  });
+
+  it("renders HTML tables with multiple header rows", () => {
+    const ast: SnootyNode = {
+      type: "root",
+      position: { start: { line: 0 } },
+      children: [
+        {
+          type: "directive",
+          children: [
+            {
+              type: "list",
+              children: [
+                {
+                  type: "listItem",
+                  children: [
+                    {
+                      type: "list",
+                      children: [
+                        {
+                          type: "listItem",
+                          children: [
+                            {
+                              type: "paragraph",
+                              children: [
+                                {
+                                  type: "text",
+                                  value: "h1",
+                                },
+                              ],
+                            },
+                          ],
+                        },
+                        {
+                          type: "listItem",
+                          children: [
+                            {
+                              type: "paragraph",
+                              children: [
+                                {
+                                  type: "text",
+                                  value: "h2",
+                                },
+                              ],
+                            },
+                          ],
+                        },
+                      ],
+                    },
+                  ],
+                },
+                {
+                  type: "listItem",
+                  children: [
+                    {
+                      type: "list",
+                      children: [
+                        {
+                          type: "listItem",
+                          children: [
+                            {
+                              type: "paragraph",
+                              children: [
+                                {
+                                  type: "text",
+                                  value: "h3",
+                                },
+                              ],
+                            },
+                          ],
+                        },
+                        {
+                          type: "listItem",
+                          children: [
+                            {
+                              type: "paragraph",
+                              children: [
+                                {
+                                  type: "text",
+                                  value: "h4",
+                                },
+                              ],
+                            },
+                          ],
+                        },
+                      ],
+                    },
+                  ],
+                },
+                {
+                  type: "listItem",
+                  children: [
+                    {
+                      type: "list",
+                      children: [
+                        {
+                          type: "listItem",
+                          children: [
+                            {
+                              type: "paragraph",
+                              children: [
+                                {
+                                  type: "text",
+                                  value: "d1",
+                                },
+                              ],
+                            },
+                          ],
+                        },
+                        {
+                          type: "listItem",
+                          children: [
+                            {
+                              type: "paragraph",
+                              children: [
+                                {
+                                  type: "text",
+                                  value: "d2",
+                                },
+                              ],
+                            },
+                          ],
+                        },
+                      ],
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+          name: "list-table",
+          options: {
+            "header-rows": 2,
+          },
+        },
+      ],
+    };
+    const result = snootyAstToMd(ast, {
+      baseUrl: "/",
+    });
+    const expected = `<table>
+<tr>
+<th>
+h1
+
+</th>
+<th>
+h2
+
+</th>
+</tr>
+<tr>
+<th>
+h3
+
+</th>
+<th>
+h4
+
+</th>
+</tr>
+<tr>
+<td>
+d1
+
+</td>
+<td>
+d2
+
+</td>
+</tr>
+</table>`;
+    expect(result).toBe(expected);
+  });
+
   describe("getTitleFromSnootyAst", () => {
     it("extracts a title", () => {
       expect(getTitleFromSnootyAst(samplePage.data.ast)).toBe(

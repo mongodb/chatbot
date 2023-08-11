e08c966992f0724eef6673d05cd9dd98075c24cf
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2023-08-11 15:22:48 -0400

    add test using framework

--- chat-server/src/jestSetUp.ts
+++ chat-server/src/jestSetUp.ts
@@ -1,6 +1,5 @@
 import { logger } from "chat-core";
 import { meetsChatQualityStandards } from "./llmQualitativeTests/meetsChatQualityStandardsJestExtension";
-
 // silence logger for tests
 logger.transports.forEach((t) => (t.silent = true));
 expect.extend({

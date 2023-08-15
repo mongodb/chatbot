53d042f56e3008b3a25a745791d319e9e1a87cfa
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2023-08-15 16:53:47 -0400

    refactor removeFrontMatter to use  package

--- chat-core/src/removeFrontMatter.ts
+++ chat-core/src/removeFrontMatter.ts
@@ -1,4 +1,6 @@
 import frontmatter from "front-matter";
 export function removeFrontMatter(content: string): string {
-  return content.replace(/---\n([\s\S]*?)\n---/, "").trimStart();
+  const noFrontMatterContent = frontmatter(content.trimStart()).body;
+
+  return noFrontMatterContent.trimStart();
 }

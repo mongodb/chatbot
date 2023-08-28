29eb205bb346025c653c7ba2211db306d447170c
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2023-08-28 16:15:21 -0400

    remove dereferencing spec

--- ingest/src/chunkOpenApiSpecYaml.test.ts
+++ ingest/src/chunkOpenApiSpecYaml.test.ts
@@ -5,7 +5,7 @@ import { handlePage } from "./SnootyDataSource";
 import { chunkOpenApiSpecYaml } from "./chunkOpenApiSpecYaml";
 
 describe("chunkRedocOpenApiSpecYaml()", () => {
-  it("chunks a Redoc OpenAPI spec", async () => {
+  it("chunks a local Redoc OpenAPI spec", async () => {
     const apiSpecPage = JSON.parse(
       fs.readFileSync(
         Path.resolve(__dirname, "./test_data/localOpenApiSpecPage.json"),
@@ -22,7 +22,7 @@ describe("chunkRedocOpenApiSpecYaml()", () => {
       chunkOverlap: 0,
       tokenizer: new GPT3Tokenizer({ type: "gpt3" }),
     });
-    expect(chunks).toHaveLength(40);
+    expect(chunks).toHaveLength(24);
     const chunkTextExpected1 = `---
 resourceName: POST /action/findOne
 openApiSpec: true
@@ -38,5 +38,24 @@ baseUrls:
     description: Find a single document that matches a query.
     x-codeSamples:`;
     expect(chunks[0].text).toContain(chunkTextExpected2);
+  });
+  test("chunks a remote Redoc OpenAPI spec", async () => {
+    const apiSpecPage = JSON.parse(
+      fs.readFileSync(
+        Path.resolve(__dirname, "./test_data/remoteOpenApiSpecPage.json"),
+        "utf-8"
+      )
+    );
+    const result = await handlePage(apiSpecPage.data, {
+      sourceName: "sample-source",
+      baseUrl: "https://example.com",
+      tags: ["a"],
+    });
+    const chunks = await chunkOpenApiSpecYaml(result, {
+      chunkSize: 1250,
+      chunkOverlap: 0,
+      tokenizer: new GPT3Tokenizer({ type: "gpt3" }),
+    });
+    expect(chunks.length).toBeGreaterThan(0);
   });
 });

1b04545fad3462229a20fcbe039035db72c7a71b
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2023-09-05 16:44:12 -0400

    add bypass WAF headers

--- performance-tests/src/serverLoad.ts
+++ performance-tests/src/serverLoad.ts
@@ -2,9 +2,31 @@ import http from "k6/http";
 import { check, sleep } from "k6";
 
 export const options = {
-  vus: 75, // Number of virtual users
+  vus: 50, // Number of virtual users
   duration: "60s", // Duration of the test
 };
+
+// Used to bypass the WAF
+const browserHeaders = {
+  authority: "knowledge.staging.corp.mongodb.com",
+  accept:
+    "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+  "accept-language": "en-US,en;q=0.9",
+  "cache-control": "no-cache",
+  pragma: "no-cache",
+  "sec-ch-ua":
+    '"Chromium";v="116", "Not)A;Brand";v="24", "Google Chrome";v="116"',
+  "sec-ch-ua-mobile": "?0",
+  "sec-ch-ua-platform": '"macOS"',
+  "sec-fetch-dest": "document",
+  "sec-fetch-mode": "navigate",
+  "sec-fetch-site": "none",
+  "sec-fetch-user": "?1",
+  "upgrade-insecure-requests": "1",
+  "user-agent":
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
+};
+
 const baseUrl = __ENV.BASE_URL;
 if (baseUrl === undefined) {
   throw new Error(
@@ -13,7 +35,13 @@ if (baseUrl === undefined) {
 }
 export default async function () {
   // First request to /user
-  const conversationResponse = http.post(baseUrl + "/api/v1/conversations");
+  const conversationResponse = http.post(
+    baseUrl + "/api/v1/conversations",
+    undefined,
+    {
+      headers: browserHeaders,
+    }
+  );
 
   // Check the status code for the first request
   check(conversationResponse, {
@@ -30,6 +58,7 @@ export default async function () {
   const params = {
     headers: {
       "Content-Type": "application/json",
+      ...browserHeaders,
     },
   };
 

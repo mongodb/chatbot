e76f33d0e2c96b411606be950bcef7c0070eebed
Nick Larew <nick.larew@mongodb.com>
2023-10-23 15:03:18 -0400

    WIP - add exports

--- chat-ui/src/index.tsx
+++ chat-ui/src/index.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react-refresh/only-export-components */
 
-export { Chatbot as default } from "./Chatbot";
+export { Chatbot as default, type ChatbotProps } from "./Chatbot";
 export { DocsChatbot } from "./DocsChatbot";
 export { DevCenterChatbot } from "./DevCenterChatbot";
 export {
@@ -8,4 +8,10 @@ export {
   type ConversationState,
   type Conversation,
 } from "./useConversation.tsx";
+export { useChatbot } from "./useChatbot.tsx";
+export { useChatbotContext } from "./useChatbotContext.tsx";
+export { InputBarTrigger, type InputBarTriggerProps } from "./InputBarTrigger.tsx";
+export { FloatingActionButtonTrigger } from "./FloatingActionButtonTrigger.tsx";
+export { ModalView, type ModalViewProps } from "./ModalView.tsx";
+export { type DarkModeProps, type ChatbotViewProps } from "./ChatbotView.tsx";
 export { type Role, type MessageData } from "./services/conversations.ts";

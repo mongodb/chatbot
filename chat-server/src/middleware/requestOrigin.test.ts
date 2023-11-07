7f4768220e1d1847df8a2dd3427e65e73d58303b
Nick Larew <nick.larew@mongodb.com>
2023-11-07 16:20:19 -0500

    WIP - add requestOrigin tests

--- chat-server/src/middleware/requestOrigin.test.ts
+++ chat-server/src/middleware/requestOrigin.test.ts
@@ -1,24 +1,52 @@
 import { requireRequestOrigin } from "./requestOrigin";
 import { createRequest, createResponse } from "node-mocks-http";
 
+const baseReq = {
+  body: { message: "Hello, world!" },
+  params: { conversationId: "conversation-1234" },
+  query: { stream: "true" },
+  headers: { "req-id": "request-1234" },
+  ip: "127.0.0.1",
+};
+
 describe("requireRequestOrigin", () => {
-  it("TODO - needs proper tests", () => {
-    // TODO
-    expect(true).toBe(true);
+  it("blocks any request where the Origin header is not set", async () => {
+    const req = createRequest();
+    const res = createResponse();
+    const next = jest.fn();
+
+    const middleware = requireRequestOrigin();
+    req.body = baseReq.body
+    req.params = baseReq.params
+    req.query = baseReq.query
+    req.headers = baseReq.headers
+    req.ip = baseReq.ip
+
+    await middleware(req, res, next);
+
+    expect(next).toHaveBeenCalledTimes(0);
+    expect(res.statusCode).toBe(400);
+    expect(res._getJSONData()).toEqual({
+      error: "No Origin header",
+    });
   });
-  // it("accepts a Zod schema that describes Express requests", async () => {
-  //   const req = createRequest();
-  //   const res = createResponse();
-  //   const next = jest.fn();
+  it("allows any request where the Origin header is set", async () => {
+    const req = createRequest();
+    const res = createResponse();
+    const next = jest.fn();
 
-  //   const middleware = requireRequestOrigin();
-  //   req.body = { message: "Hello, world!" };
-  //   req.params = { conversationId: "conversation-1234" };
-  //   req.query = { stream: "true" };
-  //   req.headers = { "req-id": "request-1234" };
-  //   req.ip = "127.0.0.1";
+    const middleware = requireRequestOrigin();
+    req.body = baseReq.body;
+    req.params = baseReq.params;
+    req.query = baseReq.query;
+    req.headers = {
+      ...baseReq.headers,
+      origin: "http://localhost:5173",
+    };
+    req.ip = baseReq.ip;
 
-  //   await middleware(req, res, next);
-  //   expect(next).toHaveBeenCalledTimes(1);
-  // });
+    await middleware(req, res, next);
+
+    expect(next).toHaveBeenCalledTimes(1);
+  });
 });

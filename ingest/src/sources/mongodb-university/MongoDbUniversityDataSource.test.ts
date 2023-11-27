49bb80885db45c5995a9e9e897a65f7c22ca5ccf
mongodben <90647379+mongodben@users.noreply.github.com>
2023-11-27 17:15:11 -0500

    stub out MongoDbUniversityDataSource tests

--- ingest/src/sources/mongodb-university/MongoDbUniversityDataSource.test.ts
+++ ingest/src/sources/mongodb-university/MongoDbUniversityDataSource.test.ts
@@ -1,6 +1,18 @@
+import "dotenv/config";
+jest.setTimeout(100000);
 const baseUrl = "https://api.learn.mongodb.com/rest/catalog";
+const apiKey = process.env.UNIVERSITY_DATA_API_KEY as string;
 describe("makeMongoDBUniversityDataSource()", () => {
   it("should return a DataSource", () => {
+    // TODO
+  });
+  it("should fetch all pages for a data source", async () => {
+    // TODO
+  });
+  it("should fetch all pages for a data source with a filter", async () => {
+    // TODO
+  });
+  it("should add metadata to each page in data source", async () => {
     // TODO
   });
 });

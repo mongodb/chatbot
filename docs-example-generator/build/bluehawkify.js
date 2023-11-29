7523926f476e05558efde580251aa1a996cfb218
Nick Larew <nick.larew@mongodb.com>
2023-11-29 12:34:07 -0500

    wip

--- docs-example-generator/build/bluehawkify.js
+++ docs-example-generator/build/bluehawkify.js
@@ -1,12 +1,8 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const openai_1 = require("@azure/openai");
 const mongodb_rag_core_1 = require("mongodb-rag-core");
 const fs_1 = require("fs");
-const yaml_1 = __importDefault(require("yaml"));
 const common_tags_1 = require("common-tags");
 require("dotenv/config");
 const util_1 = require("./util");
@@ -27,9 +23,11 @@ async function main() {
         console.error(`Missing argument: transformationFilePath`);
         return;
     }
-    const [inputCodeFile, transformationFile] = (await Promise.allSettled([
+    const [inputCodeFile,
+    // transformationFile
+    ] = (await Promise.allSettled([
         fs_1.promises.readFile(inputCodeFilePath, "utf8"),
-        fs_1.promises.readFile(transformationFilePath, "utf8"),
+        // fs.readFile(transformationFilePath, "utf8"),
     ])).map((x) => {
         if (x.status === "rejected") {
             throw x.reason;
@@ -37,9 +35,12 @@ async function main() {
         return x.value;
     });
     console.log(`inputCodeFilePath: ${inputCodeFilePath}`, inputCodeFile);
-    console.log(`transformationFilePath: ${transformationFilePath}`, transformationFile);
-    const transformation = yaml_1.default.parse(transformationFile);
-    console.log(`transformation:`, transformation);
+    // console.log(
+    //   `transformationFilePath: ${transformationFilePath}`,
+    //   transformationFile
+    // );
+    // const transformation: Transformation = YAML.parse(transformationFile);
+    // console.log(`transformation:`, transformation);
     const { MONGODB_CONNECTION_URI, MONGODB_DATABASE_NAME, OPENAI_API_KEY, OPENAI_EMBEDDING_MODEL_VERSION, OPENAI_EMBEDDING_DEPLOYMENT, OPENAI_ENDPOINT, } = (0, mongodb_rag_core_1.assertEnvVars)(mongodb_rag_core_1.CORE_ENV_VARS);
     // const mongodb = new MongoClient(MONGODB_CONNECTION_URI);
     try {
@@ -59,43 +60,37 @@ async function main() {
         console.log(`Setting up...`);
         const generate = makeGenerateFunc();
         console.log(`Analyzing code...`);
-        const analyzeCodeChat = [
-            (0, util_1.systemMessage)((0, common_tags_1.stripIndents) `
-        Your task is to analyze a provided code snippet and write a succinct
-        description of the code's purpose as well as its style and other
-        notable choices. Limit your response to 100 words.
-      `),
-            (0, util_1.userMessage)((0, common_tags_1.stripIndents) `
-        Analyze the following code snippet and describe its purpose:
-
-        ${sourceCode}
-      `),
-        ];
-        const analyzeCodeOutput = await generate(analyzeCodeChat);
-        if (!analyzeCodeOutput) {
-            throw new Error("Could not analyze code");
-        }
+        const analyzeCodeOutput = await summarize({ generate, sourceCode });
         console.log(`Input analysis:\n\n${analyzeCodeOutput}\n`);
-        // console.log(`Transforming code...`);
-        // const chat = [
-        //   systemMessage(
-        //     `You transform source code files into new representations as described by the user.`
-        //   ),
-        //   userMessage(stripIndents`
-        //   The source code snippet has the following description:
-        //   ${transformation.source.description}
-        //   The desired output has the following description:
-        //   ${transformation.target.description}
-        //   Here is the source code snippet:
-        //   ${sourceCode}
-        //   Now translate to the desired output.
-        // `),
-        // ];
+        console.log(`Transforming code...`);
+        const transformed = await bluehawkify({
+            generate,
+            sourceCode,
+            sourceDescription: analyzeCodeOutput,
+            targetDescription: (0, common_tags_1.stripIndents) `
+        A file that uses jest to test the provided source code.
+
+        The file MUST include:
+
+        - a \`describe()\` block with a title that matches the provided source code's title.
+
+        - assertions via the \`expect()\` function that test actual behavior and values against expected behavior and values.
+
+        - comments with Bluehawk annotations such that the output of \`bluehawk snip\` is identical to the provided source code.
+      `,
+        });
+        // const transformed = await translate({
+        //   generate,
+        //   sourceCode,
+        //   sourceDescription: analyzeCodeOutput,
+        //   targetDescription:
+        //     "A Kotlin file that contains example source code for the MongoDB Kotlin Driver. The example should match the behavior of the provided source code as closely as possible. The code should use idiomatic Kotlin features.",
+        // });
         // const output = await generate(chat);
         // if (!output) {
         //   throw new Error("Could not generate output for query");
         // }
-        // console.log(`Created output:\n\n${output}\n`);
+        console.log(`Created output:\n\n${transformed}\n`);
     }
     finally {
         // await store.close();
@@ -106,7 +101,6 @@ function makeGenerateFunc() {
     const { OPENAI_API_KEY, OPENAI_CHAT_COMPLETION_DEPLOYMENT, OPENAI_ENDPOINT } = (0, mongodb_rag_core_1.assertEnvVars)(mongodb_rag_core_1.CORE_ENV_VARS);
     const openAiClient = new openai_1.OpenAIClient(OPENAI_ENDPOINT, new openai_1.AzureKeyCredential(OPENAI_API_KEY));
     return async (messages) => {
-        console.log("generating 1", messages);
         try {
             const { choices: [{ message }], } = await openAiClient.getChatCompletions(OPENAI_CHAT_COMPLETION_DEPLOYMENT, messages);
             return message?.content ?? undefined;
@@ -116,4 +110,89 @@ function makeGenerateFunc() {
             throw err;
         }
     };
+}
+async function summarize({ generate, sourceCode, }) {
+    generate = generate ?? makeGenerateFunc();
+    const analyzeCodeChat = [
+        (0, util_1.systemMessage)((0, common_tags_1.stripIndents) `
+        Your task is to analyze a provided code snippet and write a succinct
+        description of the code's purpose as well as its style and other
+        notable choices. Limit your response to 100 words.
+      `),
+        (0, util_1.userMessage)((0, common_tags_1.stripIndents) `
+        Analyze the following code snippet and describe its purpose:
+
+        ${sourceCode}
+      `),
+    ];
+    const analyzeCodeOutput = await generate(analyzeCodeChat);
+    if (!analyzeCodeOutput) {
+        throw new Error("Could not analyze code");
+    }
+    return analyzeCodeOutput;
+}
+async function translate({ generate, sourceCode, sourceDescription, targetDescription, }) {
+    generate = generate ?? makeGenerateFunc();
+    const translateCodeChat = [
+        (0, util_1.systemMessage)((0, common_tags_1.stripIndents) `
+      You transform source code files from one programming language into another programming language.
+      Assume the provided code is correct.
+      Use idiomatic code and style conventions in the tranformed output.
+      Output only the transformed code with no additional text.
+    `),
+        (0, util_1.userMessage)((0, common_tags_1.stripIndents) `
+      The source code snippet has the following description:
+
+      ${sourceDescription}
+
+      The desired output has the following description:
+
+      ${targetDescription}
+
+      Here is the source code snippet:
+
+      ${sourceCode}
+
+      Now translate to the desired output. Return only the transformed code with no additional text.`),
+    ];
+    const output = await generate(translateCodeChat);
+    if (!output) {
+        throw new Error("Could not generate output for query");
+    }
+    return output;
+}
+async function bluehawkify({ generate, sourceCode, sourceDescription, targetDescription, }) {
+    generate = generate ?? makeGenerateFunc();
+    const bluehawkifyChat = [
+        (0, util_1.systemMessage)((0, common_tags_1.stripIndents) `
+      You transform source code files into unit test files that ensure the behavior of the provided source code.
+      The unit test files should include the provided source code rather than importing from another file or otherwise obfuscating the source code.
+      Assume the provided code is correct.
+      Use idiomatic code and style conventions in the test code.
+      Output only the test file code with no additional text.
+    `),
+        (0, util_1.userMessage)((0, common_tags_1.stripIndents) `
+      Adapt the following code snippet into a file that tests the provided source code. Make sure to import the necessary dependencies and declare any necessary types, classes, structs, etc.
+
+      The source code snippet has the following description:
+
+      ${sourceDescription}
+
+      The desired output has the following description:
+
+      ${targetDescription}
+
+      Here is the source code snippet:
+
+      ${sourceCode}
+
+      Now generate the desired output. Return only the code with no additional text.,
+    `),
+    ];
+    console.log("bluehawkifying - here's the chat", bluehawkifyChat);
+    const output = await generate(bluehawkifyChat);
+    if (!output) {
+        throw new Error("Could not bluehawkify");
+    }
+    return output;
 }
--- docs-example-generator/src/bluehawkify.ts
+++ docs-example-generator/src/bluehawkify.ts
@@ -36,10 +36,13 @@ async function main() {
     return;
   }
 
-  const [inputCodeFile, transformationFile] = (
+  const [
+    inputCodeFile,
+    // transformationFile
+  ] = (
     await Promise.allSettled([
       fs.readFile(inputCodeFilePath, "utf8"),
-      fs.readFile(transformationFilePath, "utf8"),
+      // fs.readFile(transformationFilePath, "utf8"),
     ])
   ).map((x) => {
     if (x.status === "rejected") {
@@ -49,13 +52,13 @@ async function main() {
   });
 
   console.log(`inputCodeFilePath: ${inputCodeFilePath}`, inputCodeFile);
-  console.log(
-    `transformationFilePath: ${transformationFilePath}`,
-    transformationFile
-  );
+  // console.log(
+  //   `transformationFilePath: ${transformationFilePath}`,
+  //   transformationFile
+  // );
 
-  const transformation: Transformation = YAML.parse(transformationFile);
-  console.log(`transformation:`, transformation);
+  // const transformation: Transformation = YAML.parse(transformationFile);
+  // console.log(`transformation:`, transformation);
 
   const {
     MONGODB_CONNECTION_URI,
@@ -88,55 +91,41 @@ async function main() {
     const generate = makeGenerateFunc();
 
     console.log(`Analyzing code...`);
-    const analyzeCodeChat = [
-      systemMessage(stripIndents`
-        Your task is to analyze a provided code snippet and write a succinct
-        description of the code's purpose as well as its style and other
-        notable choices. Limit your response to 100 words.
-      `),
-      userMessage(stripIndents`
-        Analyze the following code snippet and describe its purpose:
-
-        ${sourceCode}
-      `),
-    ];
-
-    const analyzeCodeOutput = await generate(analyzeCodeChat);
-    if (!analyzeCodeOutput) {
-      throw new Error("Could not analyze code");
-    }
+    const analyzeCodeOutput = await summarize({ generate, sourceCode });
 
     console.log(`Input analysis:\n\n${analyzeCodeOutput}\n`);
 
-    // console.log(`Transforming code...`);
+    console.log(`Transforming code...`);
+    const transformed = await bluehawkify({
+      generate,
+      sourceCode,
+      sourceDescription: analyzeCodeOutput,
+      targetDescription: stripIndents`
+        A file that uses jest to test the provided source code.
 
-    // const chat = [
-    //   systemMessage(
-    //     `You transform source code files into new representations as described by the user.`
-    //   ),
-    //   userMessage(stripIndents`
-    //   The source code snippet has the following description:
+        The file MUST include:
 
-    //   ${transformation.source.description}
+        - a \`describe()\` block with a title that matches the provided source code's title.
 
-    //   The desired output has the following description:
+        - assertions via the \`expect()\` function that test actual behavior and values against expected behavior and values.
 
-    //   ${transformation.target.description}
-
-    //   Here is the source code snippet:
-
-    //   ${sourceCode}
-
-    //   Now translate to the desired output.
-    // `),
-    // ];
+        - comments with Bluehawk annotations such that the output of \`bluehawk snip\` is identical to the provided source code.
+      `,
+    });
+    // const transformed = await translate({
+    //   generate,
+    //   sourceCode,
+    //   sourceDescription: analyzeCodeOutput,
+    //   targetDescription:
+    //     "A Kotlin file that contains example source code for the MongoDB Kotlin Driver. The example should match the behavior of the provided source code as closely as possible. The code should use idiomatic Kotlin features.",
+    // });
 
     // const output = await generate(chat);
     // if (!output) {
     //   throw new Error("Could not generate output for query");
     // }
 
-    // console.log(`Created output:\n\n${output}\n`);
+    console.log(`Created output:\n\n${transformed}\n`);
   } finally {
     // await store.close();
   }
@@ -144,22 +133,13 @@ async function main() {
 
 main();
 
-type Transformation = {
-  source: {
-    description: string;
-  };
-  target: {
-    description: string;
-  };
-};
-
 export type GenerateFunc = (
   messages: ChatMessage[]
 ) => Promise<string | undefined>;
 
 function makeGenerateFunc(): GenerateFunc {
   const { OPENAI_API_KEY, OPENAI_CHAT_COMPLETION_DEPLOYMENT, OPENAI_ENDPOINT } =
-  assertEnvVars(CORE_ENV_VARS);
+    assertEnvVars(CORE_ENV_VARS);
 
   const openAiClient = new OpenAIClient(
     OPENAI_ENDPOINT,
@@ -167,7 +147,6 @@ function makeGenerateFunc(): GenerateFunc {
   );
 
   return async (messages) => {
-    console.log("generating 1", messages)
     try {
       const {
         choices: [{ message }],
@@ -181,4 +160,122 @@ function makeGenerateFunc(): GenerateFunc {
       throw err;
     }
   };
+}
+
+async function summarize({
+  generate,
+  sourceCode,
+}: {
+  generate?: GenerateFunc;
+  sourceCode: string;
+}) {
+  generate = generate ?? makeGenerateFunc();
+  const analyzeCodeChat = [
+    systemMessage(stripIndents`
+        Your task is to analyze a provided code snippet and write a succinct
+        description of the code's purpose as well as its style and other
+        notable choices. Limit your response to 100 words.
+      `),
+    userMessage(stripIndents`
+        Analyze the following code snippet and describe its purpose:
+
+        ${sourceCode}
+      `),
+  ];
+
+  const analyzeCodeOutput = await generate(analyzeCodeChat);
+  if (!analyzeCodeOutput) {
+    throw new Error("Could not analyze code");
+  }
+  return analyzeCodeOutput;
+}
+
+async function translate({
+  generate,
+  sourceCode,
+  sourceDescription,
+  targetDescription,
+}: {
+  generate?: GenerateFunc;
+  sourceCode: string;
+  sourceDescription?: string;
+  targetDescription?: string;
+}) {
+  generate = generate ?? makeGenerateFunc();
+  const translateCodeChat = [
+    systemMessage(stripIndents`
+      You transform source code files from one programming language into another programming language.
+      Assume the provided code is correct.
+      Use idiomatic code and style conventions in the tranformed output.
+      Output only the transformed code with no additional text.
+    `),
+    userMessage(stripIndents`
+      The source code snippet has the following description:
+
+      ${sourceDescription}
+
+      The desired output has the following description:
+
+      ${targetDescription}
+
+      Here is the source code snippet:
+
+      ${sourceCode}
+
+      Now translate to the desired output. Return only the transformed code with no additional text.`),
+  ];
+
+  const output = await generate(translateCodeChat);
+  if (!output) {
+    throw new Error("Could not generate output for query");
+  }
+  return output;
+}
+
+async function bluehawkify({
+  generate,
+  sourceCode,
+  sourceDescription,
+  targetDescription,
+}: {
+  generate?: GenerateFunc;
+  sourceCode: string;
+  sourceDescription?: string;
+  targetDescription?: string;
+}) {
+  generate = generate ?? makeGenerateFunc();
+  const bluehawkifyChat = [
+    systemMessage(stripIndents`
+      You transform source code files into unit test files that ensure the behavior of the provided source code.
+      The unit test files should include the provided source code rather than importing from another file or otherwise obfuscating the source code.
+      Assume the provided code is correct.
+      Use idiomatic code and style conventions in the test code.
+      Output only the test file code with no additional text.
+    `),
+    userMessage(stripIndents`
+      Adapt the following code snippet into a file that tests the provided source code. Make sure to import the necessary dependencies and declare any necessary types, classes, structs, etc.
+
+      The source code snippet has the following description:
+
+      ${sourceDescription}
+
+      The desired output has the following description:
+
+      ${targetDescription}
+
+      Here is the source code snippet:
+
+      ${sourceCode}
+
+      Now generate the desired output. Return only the code with no additional text.,
+    `),
+  ];
+
+  console.log("bluehawkifying - here's the chat", bluehawkifyChat);
+
+  const output = await generate(bluehawkifyChat);
+  if (!output) {
+    throw new Error("Could not bluehawkify");
+  }
+  return output;
 }

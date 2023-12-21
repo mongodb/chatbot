19f03830a833948b8917c7d4025fb024f624bc89
Nick Larew <nick.larew@mongodb.com>
2023-12-21 14:31:54 -0500

    targetFileExtension in commands

--- packages/mongodb-artifact-generator/commands.sh
+++ packages/mongodb-artifact-generator/commands.sh
@@ -1,5 +1,5 @@
-# Kotlin
-## Docs Pages
+# Translate Docs Pages
+## Kotlin
 ./build/main.js translateDocsPage \
   --runId="kotlin-connection-troubleshooting" \
   --source="/Users/nlarew/mongodb/docs-node/source/connection-troubleshooting.txt" \
@@ -18,8 +18,7 @@
   --targetDescription="The same page but for the MongoDB Kotlin Driver" \
   --config=./build/standardConfig.js
 
-
-# C++
+## C++
 ./build/main.js translateDocsPage \
   --runId="cpp-connection-troubleshooting" \
   --source="/Users/nlarew/mongodb/docs-node/source/connection-troubleshooting.txt" \
@@ -38,7 +37,7 @@
   --targetDescription="The same page but for the MongoDB C++ Driver" \
   --config=./build/standardConfig.js
 
-# PHP
+## PHP
 ./build/main.js translateDocsPage \
   --runId="php-connection-troubleshooting" \
   --source="/Users/nlarew/mongodb/docs-node/source/connection-troubleshooting.txt" \
@@ -57,119 +56,102 @@
   --targetDescription="The same page but for the MongoDB PHP Driver" \
   --config=./build/standardConfig.js
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-./build/main.js translateDocsPage \
-  --source="/Users/nlarew/mongodb/docs-node/source/fundamentals/indexes.txt" \
-  --targetDescription="The same page but for the MongoDB C++ Driver" \
-  --config=./build/standardConfig.js
-
-
-
-
-
-
-
 ## Code Examples
 
 ./build/main.js translateCode \
   --runId="c-usage-examples-count" \
   --source="/Users/nlarew/mongodb/docs-node/source/code-snippets/usage-examples/count.js" \
   --targetDescription="The same functionality but using the MongoDB C Driver. Write idiomatic code that uses equivalent methods and functions to those in the source." \
+  --targetFileExtension="c" \
   --config=./build/standardConfig.js
 
 ./build/main.js translateCode \
   --runId="cpp-usage-examples-count" \
   --source="/Users/nlarew/mongodb/docs-node/source/code-snippets/usage-examples/count.js" \
   --targetDescription="The same functionality but using the MongoDB C++ Driver. Write idiomatic code that uses equivalent methods and functions to those in the source." \
+  --targetFileExtension="cpp" \
   --config=./build/standardConfig.js
 
 ./build/main.js translateCode \
   --runId="csharp-usage-examples-count" \
   --source="/Users/nlarew/mongodb/docs-node/source/code-snippets/usage-examples/count.js" \
   --targetDescription="The same functionality but using the MongoDB C# Driver. Write idiomatic code that uses equivalent methods and functions to those in the source." \
+  --targetFileExtension="cs" \
   --config=./build/standardConfig.js
 
 ./build/main.js translateCode \
   --runId="go-usage-examples-count" \
   --source="/Users/nlarew/mongodb/docs-node/source/code-snippets/usage-examples/count.js" \
   --targetDescription="The same functionality but using the MongoDB Go Driver. Write idiomatic code that uses equivalent methods and functions to those in the source." \
+  --targetFileExtension="go" \
   --config=./build/standardConfig.js
 
 ./build/main.js translateCode \
   --runId="java-usage-examples-count" \
   --source="/Users/nlarew/mongodb/docs-node/source/code-snippets/usage-examples/count.js" \
   --targetDescription="The same functionality but using the MongoDB Java Driver. Write idiomatic code that uses equivalent methods and functions to those in the source." \
+  --targetFileExtension="java" \
   --config=./build/standardConfig.js
 
 ./build/main.js translateCode \
   --runId="java-reactive-streams-usage-examples-count" \
   --source="/Users/nlarew/mongodb/docs-node/source/code-snippets/usage-examples/count.js" \
   --targetDescription="The same functionality but using the MongoDB Java (Reactive Streams) Driver. Write idiomatic code that uses equivalent methods and functions to those in the source." \
+  --targetFileExtension="java" \
   --config=./build/standardConfig.js
 
 ./build/main.js translateCode \
   --runId="kotlin-usage-examples-count" \
   --source="/Users/nlarew/mongodb/docs-node/source/code-snippets/usage-examples/count.js" \
   --targetDescription="The same functionality but using the MongoDB Kotlin Driver. Write idiomatic code that uses equivalent methods and functions to those in the source." \
+  --targetFileExtension="kt" \
   --config=./build/standardConfig.js
 
-./build/main.js translateCode \
-  --runId="node-js-usage-examples-count" \
-  --source="/Users/nlarew/mongodb/docs-node/source/code-snippets/usage-examples/count.js" \
-  --targetDescription="The same functionality but using the MongoDB Node.js Driver. Write idiomatic code that uses equivalent methods and functions to those in the source." \
-  --config=./build/standardConfig.js
+# ./build/main.js translateCode \
+#   --runId="node-js-usage-examples-count" \
+#   --source="/Users/nlarew/mongodb/docs-node/source/code-snippets/usage-examples/count.js" \
+#   --targetDescription="The same functionality but using the MongoDB Node.js Driver. Write idiomatic code that uses equivalent methods and functions to those in the source." \
+#   --targetFileExtension="js" \
+#   --config=./build/standardConfig.js
 
 ./build/main.js translateCode \
   --runId="php-usage-examples-count" \
   --source="/Users/nlarew/mongodb/docs-node/source/code-snippets/usage-examples/count.js" \
   --targetDescription="The same functionality but using the MongoDB PHP Driver. Write idiomatic code that uses equivalent methods and functions to those in the source." \
+  --targetFileExtension="php" \
   --config=./build/standardConfig.js
 
 ./build/main.js translateCode \
   --runId="python-usage-examples-count" \
   --source="/Users/nlarew/mongodb/docs-node/source/code-snippets/usage-examples/count.js" \
   --targetDescription="The same functionality but using the MongoDB Python Driver. Write idiomatic code that uses equivalent methods and functions to those in the source." \
+  --targetFileExtension="py" \
   --config=./build/standardConfig.js
 
 ./build/main.js translateCode \
   --runId="ruby-usage-examples-count" \
   --source="/Users/nlarew/mongodb/docs-node/source/code-snippets/usage-examples/count.js" \
   --targetDescription="The same functionality but using the MongoDB Ruby Driver. Write idiomatic code that uses equivalent methods and functions to those in the source." \
+  --targetFileExtension="rb" \
   --config=./build/standardConfig.js
 
 ./build/main.js translateCode \
   --runId="rust-usage-examples-count" \
   --source="/Users/nlarew/mongodb/docs-node/source/code-snippets/usage-examples/count.js" \
   --targetDescription="The same functionality but using the MongoDB Rust Driver. Write idiomatic code that uses equivalent methods and functions to those in the source." \
+  --targetFileExtension="rs" \
   --config=./build/standardConfig.js
 
 ./build/main.js translateCode \
   --runId="scala-usage-examples-count" \
   --source="/Users/nlarew/mongodb/docs-node/source/code-snippets/usage-examples/count.js" \
   --targetDescription="The same functionality but using the MongoDB Scala Driver. Write idiomatic code that uses equivalent methods and functions to those in the source." \
+  --targetFileExtension="sc" \
   --config=./build/standardConfig.js
 
 ./build/main.js translateCode \
   --runId="swift-usage-examples-count" \
   --source="/Users/nlarew/mongodb/docs-node/source/code-snippets/usage-examples/count.js" \
   --targetDescription="The same functionality but using the MongoDB Swift Driver. Write idiomatic code that uses equivalent methods and functions to those in the source." \
+  --targetFileExtension="swift" \
   --config=./build/standardConfig.js

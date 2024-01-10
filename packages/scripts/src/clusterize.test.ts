decbb8f620cb90cb4d6c3f2699ba5ea8d91832c3
Chris Bush <chris.bush@mongodb.com>
2024-01-10 15:38:37 -0500

    Annotate test

--- packages/scripts/src/clusterize.test.ts
+++ packages/scripts/src/clusterize.test.ts
@@ -3,6 +3,8 @@ import { clusterize } from "./clusterize";
 describe("clusterize", () => {
   it("clusterizes nodes", () => {
     const { clusters, noise } = clusterize(
+      // Each element in this dataset represents a node with a name and a
+      // 3-dimensional position vector ("v")
       [
         {
           name: "a",
@@ -10,28 +12,30 @@ describe("clusterize", () => {
         },
         {
           name: "b",
-          v: [1, 1, 1],
+          v: [1, 1, 1], // Close to A
         },
         {
           name: "c",
-          v: [100, 1, 1],
+          v: [100, 1, 1], // Not close to A or B (~100 units away on X axis)
         },
         {
           name: "d",
-          v: [100, 1.5, 1],
+          v: [100, 1.5, 1], // Close to C
         },
         {
           name: "e",
-          v: [100, 1.5, 1.5],
+          v: [100, 1.5, 1.5], // Close to C and D
         },
         {
           name: "f",
-          v: [100, 100, 100],
+          v: [100, 100, 100], // Not close to any other node
         },
       ],
-      ({ v }) => v,
+      ({ v }) => v, // Getter function returns vector field from data object
       { epsilon: 1 }
     );
+
+    // Expect clusters [a, b], [c, d, e] and noise f
     expect(clusters).toHaveLength(2);
     expect(clusters[0]).toHaveLength(2);
     expect(clusters[0][0].name).toBe("a");

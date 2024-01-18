e3dbeeb21c0b09fce8e8635f75335717d494dfe3
Chris Bush <chris.bush@mongodb.com>
2024-01-17 19:02:15 -0500

    Create (and use) shared .eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,13 +4,40 @@ module.exports = {
   },
   root: true,
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint", "jsdoc"],
+  plugins: ["@typescript-eslint", "jest", "jsdoc"],
   extends: [
     "eslint:recommended",
     "prettier",
     "plugin:prettier/recommended",
     "plugin:@typescript-eslint/recommended",
   ],
-  ignorePatterns: ["**/build/*"],
-  rules: { "jsdoc/require-asterisk-prefix": ["error", "never"] },
+  ignorePatterns: ["build/*"],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  rules: {
+    "jsdoc/require-asterisk-prefix": ["error", "never"],
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        varsIgnorePattern: "^_",
+        argsIgnorePattern: "^_",
+        ignoreRestSiblings: true,
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ["test/**/*.ts", "*.test.ts"],
+      env: {
+        "jest/globals": true,
+      },
+    },
+  ],
+  settings: {
+    jest: {
+      version: 29,
+    },
+  },
 };
--- packages/chatbot-server-mongodb-public/.eslintrc.js
+++ packages/chatbot-server-mongodb-public/.eslintrc.js
@@ -1,42 +1,4 @@
+const baseConfig = require("../../.eslintrc.cjs");
 module.exports = {
-  env: {
-    node: true,
-  },
-  root: true,
-  parser: "@typescript-eslint/parser",
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:jest/recommended",
-    "prettier",
-  ],
-  ignorePatterns: ["build/", "node_modules/"],
-  plugins: ["@typescript-eslint", "jest"],
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: "module",
-  },
-  rules: {
-    "@typescript-eslint/no-unused-vars": [
-      "warn",
-      {
-        varsIgnorePattern: "^_",
-        argsIgnorePattern: "^_",
-        ignoreRestSiblings: true,
-      },
-    ],
-  },
-  overrides: [
-    {
-      files: ["test/**/*.ts"],
-      env: {
-        "jest/globals": true,
-      },
-    },
-  ],
-  settings: {
-    jest: {
-      version: 29,
-    },
-  },
+  ...baseConfig,
 };

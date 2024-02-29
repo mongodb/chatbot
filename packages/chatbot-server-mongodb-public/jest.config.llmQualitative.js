d753e3b83e6b2a606b2819c52dd8abde5a6af4db
mongodben <90647379+mongodben@users.noreply.github.com>
2024-02-29 12:01:58 -0500

    Remove old qualitative test suite

--- packages/chatbot-server-mongodb-public/jest.config.llmQualitative.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  preset: "ts-jest",
-  setupFiles: ["<rootDir>/src/jestEnvSetUp.ts"],
-  setupFilesAfterEnv: ["<rootDir>/src/jestSetUp.ts"],
-  testMatch: ["<rootDir>/src/llmQualitativeTests/**/*.test.ts"],
-};

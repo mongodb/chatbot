35ad544381943e13bc33986445de8ea44fca17f0
Chris Bush <chris.bush@mongodb.com>
2024-04-05 17:25:27 -0400

    Implement score reconstruction

--- packages/scripts/src/main/upgradeFaqEntriesMain.ts
+++ packages/scripts/src/main/upgradeFaqEntriesMain.ts
@@ -2,23 +2,26 @@ import { MongoClient } from "mongodb";
 import { assertEnvVars } from "mongodb-rag-core";
 import { FaqEntry } from "../findFaq";
 import { upgradeFaqEntries, FaqEntryV0 } from "../upgradeFaqEntries";
+import { ScrubbedMessage } from "../ScrubbedMessage";
 
 import "dotenv/config";
 
 const {
   FROM_CONNECTION_URI,
   FROM_DATABASE_NAME,
-  FROM_COLLECTION_NAME,
+  FROM_FAQ_COLLECTION_NAME,
+  FROM_SCRUBBED_COLLECTION_NAME,
   TO_CONNECTION_URI,
   TO_DATABASE_NAME,
-  TO_COLLECTION_NAME,
+  TO_FAQ_COLLECTION_NAME,
 } = assertEnvVars({
   FROM_CONNECTION_URI: "",
   FROM_DATABASE_NAME: "",
-  FROM_COLLECTION_NAME: "",
+  FROM_FAQ_COLLECTION_NAME: "",
+  FROM_SCRUBBED_COLLECTION_NAME: "",
   TO_CONNECTION_URI: "",
   TO_DATABASE_NAME: "",
-  TO_COLLECTION_NAME: "",
+  TO_FAQ_COLLECTION_NAME: "",
 });
 
 async function main() {
@@ -27,11 +30,15 @@ async function main() {
     const toClient = await MongoClient.connect(TO_CONNECTION_URI);
     try {
       const fromDb = fromClient.db(FROM_DATABASE_NAME);
-      const fromCollection =
-        fromDb.collection<FaqEntryV0>(FROM_COLLECTION_NAME);
+      const fromCollection = fromDb.collection<FaqEntryV0>(
+        FROM_FAQ_COLLECTION_NAME
+      );
+      const scrubbedCollection = fromDb.collection<ScrubbedMessage>(
+        FROM_SCRUBBED_COLLECTION_NAME
+      );
 
       const toDb = toClient.db(TO_DATABASE_NAME);
-      const toCollection = toDb.collection<FaqEntry>(TO_COLLECTION_NAME);
+      const toCollection = toDb.collection<FaqEntry>(TO_FAQ_COLLECTION_NAME);
 
       const entries = await fromCollection
         .find({
@@ -39,10 +46,17 @@ async function main() {
         })
         .toArray();
 
-      // Associate each question with an ID to track questions across runs of
-      // findFaq
       const upgradedEntries = await upgradeFaqEntries({
         entries,
+        async countUserMessages({ from, to }) {
+          return await scrubbedCollection.countDocuments({
+            $and: [
+              { role: "user" },
+              { createdAt: { $gte: from } },
+              { createdAt: { $lte: to } },
+            ],
+          });
+        },
       });
 
       const bulkWriteResult = await toCollection.bulkWrite(
--- packages/scripts/src/upgradeFaqEntries.ts
+++ packages/scripts/src/upgradeFaqEntries.ts
@@ -1,4 +1,4 @@
-import { WithId } from "mongodb";
+import { WithId, ObjectId } from "mongodb";
 import { ResponseMessage } from "./findFaq";
 import { UserMessage } from "mongodb-chatbot-server";
 import { FaqEntry, randomlySampleQuestions } from "./findFaq";
@@ -27,7 +27,7 @@ export type FaqEntryV0 = {
   /**
     The original question user messages.
    */
-  questions: UserMessage[];
+  questions: (UserMessage & { createdAt: Date })[];
 
   /**
     The original response(s) to the user message.
@@ -46,6 +46,11 @@ export type FaqEntryV0 = {
   faqId?: string;
 };
 
+export type CountUserMessagesFunc = (args: {
+  from: Date;
+  to: Date;
+}) => Promise<number>;
+
 const deletedProps: Exclude<keyof FaqEntryV0, keyof FaqEntry>[] = [
   "embeddings",
   "questions",
@@ -54,29 +59,83 @@ const deletedProps: Exclude<keyof FaqEntryV0, keyof FaqEntry>[] = [
 
 export const upgradeFaqEntries = async ({
   entries,
+  countUserMessages,
 }: {
   entries: WithId<FaqEntryV0>[];
+  countUserMessages: CountUserMessagesFunc;
 }): Promise<WithId<FaqEntry>[]> => {
-  return entries
-    .filter(
-      // Only upgrade when schema version is below 1
-      ({ schemaVersion }) => schemaVersion === undefined || schemaVersion < 1
-    )
-    .map((entry) => {
-      const { questions } = entry;
-      const [question, ...sampleOriginals] = randomlySampleQuestions(
-        questions.map(({ content }) => content)
-      );
-      const upgradedEntry: WithId<FaqEntry> &
-        Partial<Omit<WithId<FaqEntryV0>, "schemaVersion">> = {
-        ...entry,
-        schemaVersion: 1,
-        instanceCount: 0, // No real way to restore instanceCount/snapshotTotal from faqScore
-        snapshotTotal: 0,
-        sampleOriginals,
-        question,
-      };
-      deletedProps.forEach((k) => delete upgradedEntry[k]);
-      return upgradedEntry;
+  return await Promise.all(
+    entries
+      .filter(
+        // Only upgrade when schema version is below 1
+        ({ schemaVersion }) => schemaVersion === undefined || schemaVersion < 1
+      )
+      .map(async (entry) => {
+        const { questions, faqScore } = entry;
+        const [question, ...sampleOriginals] = randomlySampleQuestions(
+          questions.map(({ content }) => content)
+        );
+
+        const { instanceCount, snapshotTotal } = await reconstructCounts({
+          questions,
+          countUserMessages,
+          faqScore,
+          entryId: entry._id,
+        });
+
+        const upgradedEntry: WithId<FaqEntry> &
+          Partial<Omit<WithId<FaqEntryV0>, "schemaVersion">> = {
+          ...entry,
+          schemaVersion: 1,
+          instanceCount,
+          snapshotTotal,
+          sampleOriginals,
+          question,
+        };
+        deletedProps.forEach((k) => delete upgradedEntry[k]);
+        return upgradedEntry;
+      })
+  );
+};
+
+export const reconstructCounts = async ({
+  entryId,
+  questions,
+  countUserMessages,
+  faqScore,
+}: {
+  entryId: ObjectId;
+  questions: (UserMessage & { createdAt: Date })[];
+  countUserMessages: CountUserMessagesFunc;
+  faqScore: number;
+}) => {
+  try {
+    const [minCreatedAt, maxCreatedAt] = questions.reduce(
+      (acc, { createdAt }) => {
+        const minTime = acc[0].getTime();
+        const maxTime = acc[0].getTime();
+        const curTime = createdAt.getTime();
+        return [
+          curTime < minTime ? createdAt : acc[0],
+          curTime > maxTime ? createdAt : acc[1],
+        ];
+      },
+      [questions[0].createdAt, questions[0].createdAt]
+    );
+
+    const snapshotTotal = await countUserMessages({
+      from: minCreatedAt,
+      to: maxCreatedAt,
     });
+
+    // faqScore = instanceCount / snapshotTotal
+    // -> instanceCount = faqScore * snapshotTotal
+    const instanceCount = Math.floor(faqScore * snapshotTotal);
+    return { snapshotTotal, instanceCount };
+  } catch (error) {
+    console.error(
+      `Failed to reconstruct counts for ${entryId}: ${(error as Error).message}`
+    );
+    return { snapshotTotal: -1, instanceCount: -1 };
+  }
 };

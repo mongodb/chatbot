6310a87b96e9c25e51352d0f269f32db11bbeaf7
mongodben <90647379+mongodben@users.noreply.github.com>
2024-05-09 12:29:59 -0400

    include landing now that fixed

--- packages/ingest-mongodb-public/src/sources/snooty.test.ts
+++ packages/ingest-mongodb-public/src/sources/snooty.test.ts
@@ -5,9 +5,7 @@ import {
 } from "./snooty";
 
 describe("Snooty data sources", () => {
-  // Skip the "landing" site to avoid an issue with the Snooty Data API. Remove this filter when the issue is fixed.
-  const projectConfigs = snootyProjectConfig.filter(p => p.name !== "landing");
-  test.each(projectConfigs)("$name should fetch data", async (project) => {
+  test.each(snootyProjectConfig)("$name should fetch data", async (project) => {
     const [source] = await makeSnootyDataSources(snootyDataApiBaseUrl, [
       project,
     ]);

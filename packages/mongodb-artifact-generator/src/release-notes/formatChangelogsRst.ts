a9e2dcc03bd4a602c0881083bbcf097ddaad7ee2
Nick Larew <nick.larew@mongodb.com>
2024-06-20 15:04:35 -0400

    format rst WIP

---
+++ packages/mongodb-artifact-generator/src/release-notes/formatChangelogsRst.ts
@@ -0,0 +1,28 @@
+import { ClassifiedChangelog } from "./classifyChangelog";
+
+export function formatChangelogsRst(changelogs: ClassifiedChangelog[]) {
+  // Filter out internal changes
+  const externalChangelogs = changelogs.filter(
+    (c) => c.audience.type !== "internal"
+  );
+  // Group by scope
+  const groupedChangelogs = externalChangelogs.reduce((acc, c) => {
+    if (!acc[c.scope.type]) {
+      acc[c.scope.type] = [];
+    }
+    acc[c.scope.type].push(c);
+    return acc;
+  }, {} as Record<string, ClassifiedChangelog[]>);
+  // Format each group
+  const formattedChangelogs = Object.entries(groupedChangelogs)
+    .map(([scope, changelogs]) => {
+      const formattedScope = `**${scope}**\n`;
+      const formattedChangelogs = changelogs.map((c) => {
+        return `- ${c.changelog}`;
+      });
+      return [formattedScope, ...formattedChangelogs].join("\n");
+    })
+    .join("\n\n");
+
+  return formattedChangelogs;
+}

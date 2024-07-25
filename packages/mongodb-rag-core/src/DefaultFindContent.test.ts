6633870b271d664b4d5f6190cc623ef42e469533
mongodben <90647379+mongodben@users.noreply.github.com>
2024-07-25 16:18:51 -0400

    update import in test

--- packages/mongodb-rag-core/src/DefaultFindContent.test.ts
+++ packages/mongodb-rag-core/src/DefaultFindContent.test.ts
@@ -1,11 +1,37 @@
-// TODO: update these
-import {
-  embedder,
-  embeddedContentStore,
-} from "mongodb-chatbot-server/src/test/testConfig";
+import { AzureKeyCredential, OpenAIClient } from "@azure/openai";
 import { makeDefaultFindContent } from "./DefaultFindContent";
+import { makeMongoDbEmbeddedContentStore } from "./MongoDbEmbeddedContentStore";
+import { makeOpenAiEmbedder } from "./OpenAiEmbedder";
+import { assertEnvVars } from "./assertEnvVars";
+import { CORE_ENV_VARS } from "./CoreEnvVars";
 
 describe("makeDefaultFindContent()", () => {
+  const {
+    MONGODB_CONNECTION_URI,
+    MONGODB_DATABASE_NAME,
+    OPENAI_ENDPOINT,
+    OPENAI_API_KEY,
+    OPENAI_EMBEDDING_DEPLOYMENT,
+  } = assertEnvVars(CORE_ENV_VARS);
+  const embeddedContentStore = makeMongoDbEmbeddedContentStore({
+    connectionUri: MONGODB_CONNECTION_URI,
+    databaseName: MONGODB_DATABASE_NAME,
+  });
+
+  const openAiClient = new OpenAIClient(
+    OPENAI_ENDPOINT,
+    new AzureKeyCredential(OPENAI_API_KEY)
+  );
+
+  const embedder = makeOpenAiEmbedder({
+    openAiClient,
+    deployment: OPENAI_EMBEDDING_DEPLOYMENT,
+    backoffOptions: {
+      numOfAttempts: 3,
+      maxDelay: 5000,
+    },
+  });
+
   const findContent = makeDefaultFindContent({
     embedder,
     store: embeddedContentStore,

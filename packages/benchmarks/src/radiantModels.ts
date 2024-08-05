242cb729f7d318bc74971baf15f94ee821930f6a
mongodben <90647379+mongodben@users.noreply.github.com>
2024-08-05 10:59:50 -0400

    Add additional models

--- packages/benchmarks/src/radiantModels.ts
+++ packages/benchmarks/src/radiantModels.ts
@@ -1,7 +1,6 @@
 /**
   @fileoverview This file contains the list of LLMs that are available through Radiant.
  */
-// TODO: get more models
 export const radiantModels: {
   label: string;
   radiantModelDeployment: string;
@@ -18,9 +17,36 @@ export const radiantModels: {
     label: "gpt-4o",
     radiantModelDeployment: "gpt-4o-eai-experimentation",
   },
-  // TODO: mistral seems broken
-  // {
-  //   label: "mistral-large",
-  //   radiantModelDeployment: "Mistral-large-eai",
-  // },
+  {
+    label: "claude-3-sonnet",
+    radiantModelDeployment: "anthropic.claude-3-sonnet-20240229-v1:0",
+  },
+  {
+    label: "claude-3-haiku",
+    radiantModelDeployment: "anthropic.claude-3-haiku-20240307-v1:0",
+  },
+  {
+    label: "claude-35-sonnet",
+    radiantModelDeployment: "anthropic.claude-3-5-sonnet-20240620-v1:0",
+  },
+  {
+    label: "llama-3-70b",
+    radiantModelDeployment: "meta.llama3-70b-instruct-v1:0",
+  },
+  {
+    label: "gemini-1.5-pro-preview",
+    radiantModelDeployment: "gemini-1.5-pro-preview-0409",
+  },
+  {
+    label: "gemini-1.5-pro-preview",
+    radiantModelDeployment: "gemini-1.5-pro-preview-0409",
+  },
+  {
+    label: "gemini-1.0-pro",
+    radiantModelDeployment: "gemini-1.0-pro-001",
+  },
+  {
+    label: "gemini-1.0-pro-vision",
+    radiantModelDeployment: "gemini-1.0-pro-vision-001",
+  },
 ];

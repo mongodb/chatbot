57c98f60d08c6fb065bad801c6da920af2cf009b
mongodben <90647379+mongodben@users.noreply.github.com>
2024-08-13 12:05:45 -0400

    call radiant thru openai proxy endpoint

--- packages/benchmarks/src/makeRadiantChatLlm.ts
+++ packages/benchmarks/src/makeRadiantChatLlm.ts
@@ -1,5 +1,5 @@
-import { makeOpenAiChatLlm } from "mongodb-chatbot-server";
-
+import { ChatOpenAI } from "@langchain/openai";
+import { makeLangchainChatLlm } from "mongodb-chatbot-server";
 export const makeRadiantChatLlm = async ({
   endpoint,
   apiKey,
@@ -7,42 +7,32 @@ export const makeRadiantChatLlm = async ({
   mongoDbAuthCookie,
   lmmConfigOptions = {
     temperature: 0,
+    maxTokens: 2000,
   },
 }: {
   endpoint: string;
   apiKey: string;
   deployment: string;
   mongoDbAuthCookie?: string;
-  lmmConfigOptions: {
-    temperature: number;
+  lmmConfigOptions?: {
+    temperature?: number;
+    maxTokens?: number;
   };
 }) => {
-  const { AzureKeyCredential, OpenAIClient } = await import("@azure/openai");
-  return makeOpenAiChatLlm({
-    deployment,
-    openAiLmmConfigOptions: lmmConfigOptions,
-    openAiClient: new OpenAIClient(endpoint, new AzureKeyCredential(apiKey), {
-      // Allow insecure connection when in staging/production
-      // b/c connecting w/in the same k8s cluster
-      allowInsecureConnection:
-        process.env.NODE_ENV === "production" ||
-        process.env.NODE_ENV === "staging",
-      // If connecting to Radiant over the internet,
-      // you must include a MongoDB CorpSecure cookie in the request.
-      additionalPolicies: [
-        {
-          position: "perCall",
-          policy: {
-            name: "add-cookie",
-            sendRequest(request, next) {
-              if (mongoDbAuthCookie) {
-                request.headers.set("Cookie", mongoDbAuthCookie);
-              }
-              return next(request);
-            },
-          },
-        },
-      ],
-    }),
+  return makeLangchainChatLlm({
+    chatModel: new ChatOpenAI({
+      apiKey,
+      modelName: deployment,
+      temperature: lmmConfigOptions.temperature,
+      maxTokens: lmmConfigOptions.maxTokens,
+      configuration: {
+        baseURL: endpoint,
+        defaultHeaders: mongoDbAuthCookie
+          ? {
+              Cookie: mongoDbAuthCookie,
+            }
+          : undefined,
+      },
+    }) as unknown as Parameters<typeof makeLangchainChatLlm>[0]["chatModel"],
   });
 };

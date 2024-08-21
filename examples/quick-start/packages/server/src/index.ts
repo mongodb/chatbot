97b0ae91cde27df1316bc4e88cb7f01ec1696b25
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2024-08-21 09:43:04 -0400

    Update examples/quick-start/packages/server/src/index.ts

--- examples/quick-start/packages/server/src/index.ts
+++ examples/quick-start/packages/server/src/index.ts
@@ -83,7 +83,7 @@ const findContent = makeDefaultFindContent({
 const makeUserMessage: MakeUserMessageFunc = async function ({
   content,
   originalUserMessage,
-}): Promise<UserMessage> {
+}) {
   const chunkSeparator = "~~~~~~";
   const context = content.map((c) => c.text).join(`\n${chunkSeparator}\n`);
   const contentForLlm = `Using the following information, answer the user query.

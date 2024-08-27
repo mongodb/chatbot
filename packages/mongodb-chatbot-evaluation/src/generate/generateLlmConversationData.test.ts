312b922c0f52520ce856484c3a1b0d46fd9e230a
Nick Larew <nick.larew@mongodb.com>
2024-08-27 11:23:28 -0400

    (EAI-484) Fix broken langchain chat model wrapper (#478)

--- packages/mongodb-chatbot-evaluation/src/generate/generateLlmConversationData.test.ts
+++ packages/mongodb-chatbot-evaluation/src/generate/generateLlmConversationData.test.ts
@@ -1,6 +1,6 @@
 import { FakeListChatModel } from "@langchain/core/utils/testing";
 import { makeGenerateLlmConversationData } from "./generateLlmConversationData";
-import { MakeLangchainChatLlmProps, ObjectId } from "mongodb-rag-core";
+import { ObjectId } from "mongodb-rag-core";
 import { testCases, triggerErrorTestCases } from "../test/mockTestCases";
 import { ChatOpenAI } from "@langchain/openai";
 import { makeLangchainChatLlm } from "mongodb-chatbot-server";
@@ -38,8 +38,7 @@ describe("makeGenerateLlmConversationData", () => {
     });
     const generateLlmConversationData = makeGenerateLlmConversationData({
       chatLlm: makeLangchainChatLlm({
-        chatModel:
-          throwingLlm as unknown as MakeLangchainChatLlmProps["chatModel"],
+        chatModel: throwingLlm,
       }),
       backOffOptions: {
         numOfAttempts: 1,

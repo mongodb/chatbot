a5bb1909cac97a8197f1e2a65ffb1c0ddcc68574
mongodben <90647379+mongodben@users.noreply.github.com>
2024-09-18 16:19:22 -0400

    tests passing + code complete

--- packages/benchmarks/src/textToDriver/generateDriverCode/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./makeTextToDriverPrompt";
-export * from "./node";
--- packages/benchmarks/src/textToDriver/generateDriverCode/makeTextToDriverPrompt.ts
+++ packages/benchmarks/src/textToDriver/generateDriverCode/makeTextToDriverPrompt.ts
@@ -1,12 +1,35 @@
 import { Document, IndexDescription } from "mongodb-rag-core";
 import { OpenAI } from "openai";
+import { parseSchema } from "mongodb-schema";
+import { strict as assert } from "assert";
 
-type ChatCompletionMessageParam =
-  OpenAI.Chat.Completions.ChatCompletionMessageParam;
+export type ChatCompletionMessageParam = OpenAI.Chat.ChatCompletionMessageParam;
+export type ChatCompletionSystemMessageParam =
+  OpenAI.Chat.Completions.ChatCompletionSystemMessageParam & {
+    content: string;
+  };
+export interface TextToDriverPromptParams {
+  /**
+    Custom instructions to include in the system prompt.
+    These are included at the top of the system prompt.
+    Can include things like chain-of-thought instructions
+    or specific details about how the response should be formatted.
 
-export interface BuildPromptParams {
-  fewShotExamples: ChatCompletionMessageParam[];
+    While not strictly necessary, it is strongly recommended to include
+    if you would like to produce reliably executable code output.
+   */
+  customInstructions?: string;
 
+  /**
+    Few-shot example prompt and response messages
+    to include along with the system prompt.
+    Help guide the model to generate responses in the correct format.
+   */
+  fewShotExamples?: ChatCompletionMessageParam[];
+
+  /**
+    MongoDB database information to include in the system prompt.
+   */
   mongoDb: {
     /**
       Generate collection schemas for the system prompt
@@ -20,25 +43,37 @@ export interface BuildPromptParams {
       Currently, only one database is supported.
      */
     databaseName: string;
-    collections: {
-      /**
-        Name of collection to use in queries.
-       */
-      collectionName: string;
-      indexes?: IndexDescription[];
-      /**
-          Example documents to include in the system prompt.
-          These are also used to generate a collection schema
-          that is included in the system prompt
-          if `generateCollectionSchemas: true`.
-        */
-      exampleDocuments?: Document[];
-    }[];
+
+    /**
+      Information on collections to include in the system prompt.
+
+      You must include at least one collection per database.
+     */
+    collections: [CollectionInfo, ...CollectionInfo[]];
   };
+}
+
+export interface CollectionInfo {
   /**
-    Driver-specific instructions to include in the system prompt.
+    Name of collection to use in queries.
    */
-  systemPromptInstructions?: string;
+  collectionName: string;
+
+  /**
+    Information on indexes to apply to the collection.
+    If you include the indexes, the system prompt will list
+    the indexes along with the collection.
+   */
+  indexes?: IndexDescription[];
+  /**
+    Example documents to include in the system prompt.
+    These are also used to generate a collection schema
+    that is included in the system prompt
+    if `generateCollectionSchemas: true`.
+
+    You *must* include at least one example document per collection.
+   */
+  exampleDocuments: [Document, ...Document[]];
 }
 
 /**
@@ -46,13 +81,80 @@ export interface BuildPromptParams {
   to send to the LLM along with each natural language prompt.
  */
 export async function makeTextToDriverPrompt(
-  params: BuildPromptParams
-): Promise<ChatCompletionMessageParam[]> {
-  // TODO
-  return [];
+  params: TextToDriverPromptParams
+): Promise<
+  [ChatCompletionSystemMessageParam, ...ChatCompletionMessageParam[]]
+> {
+  const systemPrompt = {
+    role: "system",
+    content: await makeSystemPromptMessage(params),
+  } satisfies ChatCompletionMessageParam;
+  return [systemPrompt, ...(params.fewShotExamples ?? [])];
 }
 
-function makeSystemPrompt() {}
-function makeCollectionInformation() {
-  return ``;
+async function makeSystemPromptMessage(
+  params: Omit<TextToDriverPromptParams, "fewShotExamples">
+) {
+  let systemPromptMessage = params.customInstructions
+    ? params.customInstructions.trim() + "\n\n"
+    : "";
+  systemPromptMessage += await makeDatabasePromptContent(params.mongoDb);
+
+  return systemPromptMessage;
+}
+
+export const COLLECTIONS_REQUIRED_ERROR = "At least one collection is required";
+async function makeDatabasePromptContent(
+  params: TextToDriverPromptParams["mongoDb"]
+) {
+  assert(params.collections.length > 0, COLLECTIONS_REQUIRED_ERROR);
+  let databasePrompt =
+    "The MongoDB database has the following collections with these schemas and sample documents:\n\n";
+  for (const collection of params.collections) {
+    databasePrompt += await makeCollectionPromptContent({
+      collection,
+      generateCollectionSchema: params.generateCollectionSchemas,
+    });
+  }
+  return databasePrompt;
+}
+
+export const EXAMPLE_DOCUMENTS_REQUIRED_ERROR =
+  "Example documents are required";
+async function makeCollectionPromptContent({
+  collection,
+  generateCollectionSchema = true,
+}: {
+  collection: TextToDriverPromptParams["mongoDb"]["collections"][number];
+  generateCollectionSchema?: TextToDriverPromptParams["mongoDb"]["generateCollectionSchemas"];
+}) {
+  assert(
+    collection.exampleDocuments.length > 0,
+    EXAMPLE_DOCUMENTS_REQUIRED_ERROR
+  );
+
+  let collectionPromptContent = `## \`"${collection.collectionName}"\` collection\n\n`;
+
+  // Include schema if requested
+  if (generateCollectionSchema) {
+    const schema = await parseSchema(collection.exampleDocuments);
+
+    collectionPromptContent += `Parsed schema:
+
+${JSON.stringify(schema, null, 2)}\n\n`;
+  }
+
+  // Include collection documents
+  collectionPromptContent += `Example documents:
+
+${JSON.stringify(collection.exampleDocuments, null, 2)}\n\n`;
+
+  // Include indexes if provided
+  if (collection.indexes) {
+    collectionPromptContent += `Indexes:
+
+${JSON.stringify(collection.indexes, null, 2)}\n\n`;
+  }
+
+  return collectionPromptContent;
 }

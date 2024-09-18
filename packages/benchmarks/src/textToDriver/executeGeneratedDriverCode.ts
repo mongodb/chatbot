e0d3df860ec3dbd8fe7b0afb64106778fac8ce9a
mongodben <90647379+mongodben@users.noreply.github.com>
2024-09-18 17:07:16 -0400

    execute generated driver code from string on a db

---
+++ packages/benchmarks/src/textToDriver/executeGeneratedDriverCode.ts
@@ -0,0 +1,83 @@
+import { Document, MongoClient } from "mongodb-rag-core";
+
+export interface ExecuteGeneratedDriverCodeParams {
+  /**
+    MongoDB client instance to use for executing the generated driver code.
+   */
+  mongoClient: MongoClient;
+  /**
+    The generated driver code to execute.
+    @example database.collection("myCollection").find({}).toArray();
+   */
+  generatedDriverCode: string;
+  /**
+    The name of the database to use for executing the generated driver code.
+   */
+  databaseName: string;
+}
+
+export interface ExecuteGeneratedDriverCodeResult {
+  /**
+    The result of executing the generated driver code.
+    `null` if no result was returned.
+   */
+  result: Document | Document[] | number | null;
+  /**
+    An error message if an error occurred during execution.
+   */
+  error?: {
+    message: string;
+  };
+  /**
+    The time in milliseconds it took to execute the generated driver code.
+   */
+  executionTimeMs: number;
+}
+
+/**
+  Execute the generated driver code using the provided MongoDB client instance.
+
+  This function expects that the generated driver code
+  uses the `database` variable to access the MongoDB database.
+  If you do not provide a `database` variable in the generated driver code,
+  this function will not work as expected.
+
+ */
+export async function executeGeneratedDriverCode(
+  params: ExecuteGeneratedDriverCodeParams
+): Promise<{
+  result: Document | Document[] | number | null;
+  error?: {
+    message: string;
+  };
+  executionTimeMs: number;
+}> {
+  const { generatedDriverCode, mongoClient, databaseName } = params;
+  // While the database variable looks unused, it is actually used in the generated code
+  const database = mongoClient.db(databaseName);
+
+  const startTime = Date.now();
+
+  try {
+    // Wrap the generated code in an async IIFE to guarantee that it returns a Promise
+    const result = await eval(
+      `(async () => { return ${generatedDriverCode} })()`
+    );
+
+    const endTime = Date.now();
+
+    return {
+      result,
+      executionTimeMs: endTime - startTime,
+    };
+  } catch (error: any) {
+    const endTime = Date.now();
+    return {
+      result: null,
+      error: {
+        message: error.message || "An unknown error occurred",
+      },
+      executionTimeMs: endTime - startTime,
+    };
+  }
+}

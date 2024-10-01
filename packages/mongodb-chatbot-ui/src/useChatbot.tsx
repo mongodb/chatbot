21159cab86e08c7c7bcafa48ccc5d673daa825c3
Nick Larew <nick.larew@mongodb.com>
2024-10-01 16:24:58 -0500

    remove unused import

--- packages/mongodb-chatbot-ui/src/useChatbot.tsx
+++ packages/mongodb-chatbot-ui/src/useChatbot.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useRef, useState } from "react";
 import { useConversation, type UseConversationParams } from "./useConversation";
 
 export type OpenCloseHandlers = {

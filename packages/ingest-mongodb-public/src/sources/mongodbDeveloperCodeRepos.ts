979554fbdcc820c5b0b1cda89d87ffea4bca0b23
mongodben <90647379+mongodben@users.noreply.github.com>
2024-10-28 11:30:46 -0400

    master --> main

--- packages/ingest-mongodb-public/src/sources/mongodbDeveloperCodeRepos.ts
+++ packages/ingest-mongodb-public/src/sources/mongodbDeveloperCodeRepos.ts
@@ -49,7 +49,7 @@ export const leafsteroids = async () => {
     name: "mongodb-developer-leafsteroids",
     repoUrl: "https://github.com/mongodb-developer/leafsteroids",
     repoLoaderOptions: {
-      branch: "master",
+      branch: "main",
     },
     metadata: {
       productName: "MongoDB Developer Code Examples",
@@ -78,7 +78,7 @@ export const mongoRx = async () => {
     name: "mongodb-developer-mongorx",
     repoUrl: "https://github.com/mongodb-developer/mongorx",
     repoLoaderOptions: {
-      branch: "master",
+      branch: "main",
       ignoreFiles: [/\.browserslistrc/],
     },
     metadata: {

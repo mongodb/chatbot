3ce683695a4a8d3ea5d2450e44d862f243113064
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2025-01-21 11:26:42 -0500

    (EAI-625): LLM-as-a-judge evals on server messages (#593)
    * add braintrust to core
    
    * Update packages/mongodb-chatbot-server/src/routes/conversations/addMessageToConversation.ts
    
    * working server logging instrumentation
    
    * checkpoint
    
    * target more recent version to use es2023
    
    * update lib to latest
    
    * update tracing
    
    * lets see
    
    * braintrust tracing in test
    
    * no trace in test
    
    * tracing tests
    
    * up timeout on flaky test
    
    * chatbot staging
    
    * add basic LLM as a judge tracing
    
    * tracing w/ LLM as a judge
    
    * add judge vars to drone
    
    * implement NL feedback

--- packages/chatbot-server-mongodb-public/src/tracing.ts
+++ packages/chatbot-server-mongodb-public/src/tracing.ts
@@ -1,28 +1,44 @@
 import { AppConfig } from "mongodb-chatbot-server";
 import { SomeMessage, UserMessage } from "mongodb-rag-core";
 import { llmDoesNotKnowMessage } from "./systemPrompt";
+import { strict as assert } from "assert";
+import { ContextRelevancy, AnswerRelevancy, Faithfulness } from "autoevals";
+import { traced } from "mongodb-rag-core/braintrust";
 
 export const makeAddMessageToConversationUpdateTrace: (
-  k: number
+  k: number,
+  llmAsAJudge?: LlmAsAJudge
 ) => AppConfig["conversationsRouterConfig"]["addMessageToConversationUpdateTrace"] = (
-  k
-) =>
-  async function ({ traceId, addedMessages, logger }) {
+  k,
+  llmAsAJudge
+) => {
+  return async function ({ traceId, addedMessages, logger }) {
     const tracingData = extractTracingData(addedMessages);
+    const judges = shouldJudge(llmAsAJudge?.percentToJudge);
+
     logger.updateSpan({
       id: traceId,
       tags: tracingData.tags,
       scores: {
-        RejectedQuery: tracingData.rejectQuery === true ? 1 : null,
-        VerifiedAnswer: tracingData.isVerifiedAnswer === true ? 1 : null,
-        LlmDoesNotKnow: tracingData.llmDoesNotKnow === true ? 1 : null,
-        [`RetrievedChunksOver${k}`]:
-          tracingData.isVerifiedAnswer !== true
-            ? tracingData.numRetrievedChunks / k
-            : null,
+        ...getTracingScores(tracingData, k),
+        ...(judges && llmAsAJudge
+          ? await getLlmAsAJudgeScores(llmAsAJudge, tracingData)
+          : undefined),
       },
     });
   };
+};
+
+function shouldJudge(percentToJudge: number | undefined): boolean {
+  if (percentToJudge === undefined) {
+    return false;
+  }
+  assert(
+    percentToJudge >= 0 && percentToJudge <= 1,
+    `percentToJudge must be between 0 and 1. Received: ${percentToJudge}`
+  );
+  return Math.random() < percentToJudge;
+}
 
 export function extractTracingData(messages: SomeMessage[]) {
   const latestUserMessage = messages.findLast(
@@ -76,6 +92,127 @@ export function extractTracingData(messages: SomeMessage[]) {
     isVerifiedAnswer,
     llmDoesNotKnow,
     numRetrievedChunks,
+    latestUserMessage,
+    latestAssistantMessage,
+  };
+}
+
+function getTracingScores(
+  tracingData: ReturnType<typeof extractTracingData>,
+  k: number
+) {
+  return {
+    RejectedQuery: tracingData.rejectQuery === true ? 1 : null,
+    VerifiedAnswer: tracingData.isVerifiedAnswer === true ? 1 : null,
+    LlmDoesNotKnow: tracingData.llmDoesNotKnow === true ? 1 : null,
+    [`RetrievedChunksOver${k}`]:
+      tracingData.isVerifiedAnswer !== true
+        ? tracingData.numRetrievedChunks / k
+        : null,
+  };
+}
+
+interface LlmAsAJudge {
+  /**
+    Percent of numbers to judge. Between 0 and 1.
+   */
+  percentToJudge: number;
+  judgeModel: string;
+  judgeEmbeddingModel: string;
+  openAiConfig: Pick<
+    Parameters<typeof Faithfulness>[0],
+    | "azureOpenAi"
+    | "openAiApiKey"
+    | "openAiBaseUrl"
+    | "openAiDangerouslyAllowBrowser"
+    | "openAiDefaultHeaders"
+    | "openAiOrganizationId"
+  >;
+}
+const nullScore = {
+  score: null,
+};
+
+export async function getLlmAsAJudgeScores(
+  {
+    judgeModel,
+    openAiConfig,
+    judgeEmbeddingModel,
+  }: Omit<LlmAsAJudge, "percentToJudge">,
+  tracingData: ReturnType<typeof extractTracingData>
+) {
+  // Return if we don't have the necessary data to judge
+  if (
+    tracingData.isVerifiedAnswer ||
+    tracingData.rejectQuery ||
+    tracingData.numRetrievedChunks === 0 ||
+    tracingData.llmDoesNotKnow ||
+    !tracingData.latestUserMessage ||
+    !tracingData.latestAssistantMessage
+  ) {
+    return;
+  }
+  const input = tracingData.latestUserMessage.content;
+  const output = tracingData.latestAssistantMessage.content;
+  const context = tracingData.latestUserMessage.contextContent
+    ?.map((c) => c.text)
+    .filter((value): value is string => typeof value === "string");
+
+  const [faithfulness, answerRelevancy, contextRelevancy] = context
+    ? await traced(
+        async () =>
+          Promise.all([
+            traced(
+              async () =>
+                Faithfulness({
+                  input,
+                  output,
+                  context,
+                  model: judgeModel,
+                  ...openAiConfig,
+                }),
+              {
+                name: "Faithfulness",
+              }
+            ),
+            traced(
+              async () =>
+                AnswerRelevancy({
+                  input,
+                  output,
+                  context,
+                  model: judgeModel,
+                  embeddingModel: judgeEmbeddingModel,
+                  ...openAiConfig,
+                }),
+              {
+                name: "AnswerRelevancy",
+              }
+            ),
+            traced(
+              async () =>
+                ContextRelevancy({
+                  input,
+                  output,
+                  context,
+                  model: judgeModel,
+                  ...openAiConfig,
+                }),
+              {
+                name: "ContextRelevancy",
+              }
+            ),
+          ]),
+        {
+          name: "LlmAsAJudge",
+        }
+      )
+    : [nullScore, nullScore, nullScore];
+
+  return {
+    Faithfulness: faithfulness.score,
+    AnswerRelevancy: answerRelevancy.score,
+    ContextRelevancy: contextRelevancy.score,
   };
 }
 

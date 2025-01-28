2e42cf42d87d27bf331065abe652bb7a50b353a2
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2025-01-28 16:10:39 -0500

    (EAI-649): Draft code example classifier type (#595)
    * migrate classifier to core
    
    * start creating docs team classifier
    
    * configurable model
    
    * working e2e
    
    * fix build err
    
    ---------
    
    Co-authored-by: Ben Perlmutter <mongodben@mongodb.com>

--- packages/mongodb-rag-core/src/index.ts
+++ packages/mongodb-rag-core/src/index.ts
@@ -20,6 +20,7 @@ export * from "./DatabaseConnection";
 export * from "./DataStreamer";
 export * from "./logger";
 export * from "./conversations/MongoDbConversations";
+export * from "./makeClassifier";
 export * from "./References";
 export * from "./VectorStore";
 export * from "./arrayFilters";

470f1dcfafb8599b70d61920a13b25ca662e082c
Ben Perlmutter <mongodben@mongodb.com>
2025-01-28 13:07:39 -0500

    lint fix

--- packages/ingest-mongodb-public/src/sources/snooty/truncateEmbeddings.ts
+++ packages/ingest-mongodb-public/src/sources/snooty/truncateEmbeddings.ts
@@ -1,6 +1,6 @@
 export function truncateEmbeddings(body: string): string {
   const embeddingRegex =
-    /([-]?0\.\d+d?),\s*([+\-]?0\.\d+d?)(?:\s*,\s*[+\-]?0\.\d+d?)*/g;
+    /([-]?0\.\d+d?),\s*([-]?0\.\d+d?)(?:\s*,\s*[-]?0\.\d+d?)*/g;
   return body.replace(embeddingRegex, (...matches) => {
     const [_wholeEmbedding, firstFloat, secondFloat] = matches;
     return `${firstFloat}, ${secondFloat}, ...`;

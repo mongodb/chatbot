be30a9a37e5c073a67fa74ea4d9d238cdac1f368
Alla Yakubova <yakubova92@gmail.com>
2025-01-29 16:15:58 -0500

    typing and cleanup

--- packages/mongodb-rag-core/src/test/globalSetup.ts
+++ packages/mongodb-rag-core/src/test/globalSetup.ts
@@ -2,12 +2,8 @@ import { MongoMemoryServer, MongoMemoryReplSet } from "mongodb-memory-server";
 import { IP, REPLICA_SET_PORT, SERVER_PORT } from "./constants";
 
 declare global {
-  namespace NodeJS {
-    interface Global {
-      __MONGO_MEMORY_SERVER_INSTANCE: MongoMemoryServer;
-      __MONGO_MEMORY_REPLICA_SET: MongoMemoryReplSet;
-    }
-  }
+  var __MONGO_MEMORY_SERVER_INSTANCE: MongoMemoryServer;
+  var __MONGO_MEMORY_REPLICA_SET: MongoMemoryReplSet;
 }
 
 module.exports = async function () {
@@ -19,7 +15,6 @@ module.exports = async function () {
       },
     });
     global.__MONGO_MEMORY_SERVER_INSTANCE = mongoMemoryServerInstance;
-    const uri = mongoMemoryServerInstance.getUri();
     const mongoMemoryServerReplicaSet = await MongoMemoryReplSet.create({
       instanceOpts: [
         {
@@ -31,7 +26,6 @@ module.exports = async function () {
       },
     });
     global.__MONGO_MEMORY_REPLICA_SET = mongoMemoryServerReplicaSet;
-    const uriReplicaSet = mongoMemoryServerReplicaSet.getUri();
   } catch (error) {
     console.error("Error in global setup:", error);
     throw error;

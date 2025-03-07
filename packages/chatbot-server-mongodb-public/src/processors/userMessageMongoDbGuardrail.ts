0e437deb85d78597bfd926e16552d4bef8305028
Alla Yakubova <yakubova92@gmail.com>
2025-03-07 16:43:39 -0500

    Apply suggestions from code review
    change "architecture" to "software architecture" in guardrail
    
    Co-authored-by: Nick Larew <nick.larew@mongodb.com>

--- packages/chatbot-server-mongodb-public/src/processors/userMessageMongoDbGuardrail.ts
+++ packages/chatbot-server-mongodb-public/src/processors/userMessageMongoDbGuardrail.ts
@@ -31,7 +31,7 @@ const systemPrompt = stripIndents`You are an expert security-focused data labele
 
   Take into account the following criteria:
   - Reject any user query that is irrelevant to a MongoDB product, educational materials, the company MongoDB, or an area relevant to MongoDB's products and business. These relevant areas include databases, cloud services, data management, information retrieval, programming languages and concepts, and artificial intelligence (retrieval augmented generation (RAG), generative AI, semantic search, etc.).
-  - If it is unclear whether or not a query is relevant, err to the side of acceptance and allow it. For example, if something looks like an aggregation stage in the MongoDB Aggregation Framework, it is relevant. If something is about something related to programming, software engineering, or architecture, it is relevant.
+  - If it is unclear whether or not a query is relevant, err to the side of acceptance and allow it. For example, if something looks like an aggregation stage in the MongoDB Aggregation Framework, it is relevant. If something is about something related to programming, software engineering, or software architecture, it is relevant.
   - Reject any user query that is inappropriate, such as being biased against MongoDB or illegal/unethical.
 
   Your pay is determined by the accuracy of your labels as judged against other expert labelers, so do excellent work to maximize your earnings to support your family.`;

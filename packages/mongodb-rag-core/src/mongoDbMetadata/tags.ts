9988271544a69c19d861cb18efef2dc95f44b918
Alla Yakubova <yakubova92@gmail.com>
2025-04-07 14:49:36 -0400

    lint

--- packages/mongodb-rag-core/src/mongoDbMetadata/tags.ts
+++ packages/mongodb-rag-core/src/mongoDbMetadata/tags.ts
@@ -10,7 +10,7 @@ const mongoDbTopicIds = mongoDbTopics.map((topic) => topic.id);
 /**
   All possible MongoDB tags. Useful for tagging evaluations.
   */
-  export type MongoDbTag =
+export type MongoDbTag =
   | (typeof mongoDbProgrammingLanguageIds)[number]
   | (typeof mongoDbProductIds)[number]
   | (typeof mongoDbDriverIds)[number]
@@ -46,16 +46,16 @@ export const mongoDbTags = {
 } as const;
 
 /**
- * Validates an array of tag names against the MongoDbTags enum.
- * 
- * @param tagNames - An array of strings representing tag names to validate
- * @param custom - A boolean flag indicating whether custom tags are allowed
- * @throws {Error} When non-custom tags are used that don't exist in MongoDbTags enum
- * 
- * @remarks
- * If custom is false, all tags must exist in the MongoDbTags enum.
- * If any invalid tags are found, throws an error with the list of invalid tags
- * and the allowed tags from MongoDbTags enum.
+ Validates an array of tag names against the MongoDbTags enum.
+ 
+ @param tagNames - An array of strings representing tag names to validate
+ @param custom - A boolean flag indicating whether custom tags are allowed
+ @throws {Error} When non-custom tags are used that don't exist in MongoDbTags enum
+ 
+ @remarks
+ If custom is false, all tags must exist in the MongoDbTags enum.
+ If any invalid tags are found, throws an error with the list of invalid tags
+ and the allowed tags from MongoDbTags enum.
  */
 export const validateTags = (tagNames: string[], custom: boolean): void => {
   if (!custom) {
@@ -73,4 +73,4 @@ export const validateTags = (tagNames: string[], custom: boolean): void => {
       );
     }
   }
-}
+};

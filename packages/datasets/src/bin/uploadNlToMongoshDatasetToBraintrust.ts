fbaa3302a1fe8ef3ba3a83f3ee114becf66c4b05
Ben Perlmutter <mongodben@mongodb.com>
2025-04-10 09:24:19 -0400

    upload filtered

--- packages/datasets/src/bin/uploadNlToMongoshDatasetToBraintrust.ts
+++ packages/datasets/src/bin/uploadNlToMongoshDatasetToBraintrust.ts
@@ -15,20 +15,23 @@ async function main() {
   });
   const dataset = JSON.parse(
     fs.readFileSync(
-      path.resolve(dataOutDir, "filteredTextToMongoshBenchmarkResults.json"),
+      path.resolve(
+        dataOutDir,
+        "atlas_sample_data_benchmark_gpt-4o_filtered_with_execution_time.json"
+      ),
       "utf-8"
     )
-  ) as DatabaseNlQueryDatasetEntry[];
+  );
   console.log(`Total number of queries: ${dataset.length}`);
   console.log(`First query: ${JSON.stringify(dataset[0], null, 2)}`);
-  const btDataset = dataset.map(convertDatabaseNlQueryDatasetEntryToBraintrust);
   const res = await uploadDatasetToBraintrust({
     apiKey: BRAINTRUST_API_KEY,
-    datasetName: "atlas_sample_data_benchmark_gpt-4o_filtered",
+    datasetName:
+      "atlas_sample_data_benchmark_gpt-4o_filtered_with_execution_time",
     projectName: "natural-language-to-mongosh",
     description:
       "Natural language-to-Mongosh dataset filtered to only include cases where more than 1 LLM was able to answer correctly.",
-    dataset: btDataset,
+    dataset,
   });
   console.log(res);
 }

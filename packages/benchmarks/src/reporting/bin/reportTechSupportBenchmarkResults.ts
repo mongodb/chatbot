48fc3cc28bf90e64ed19d5b46bd12565b7ec679c
Ben Perlmutter <mongodben@mongodb.com>
2025-05-06 10:24:33 -0400

    change output path

--- packages/benchmarks/src/reporting/bin/reportTechSupportBenchmarkResults.ts
+++ packages/benchmarks/src/reporting/bin/reportTechSupportBenchmarkResults.ts
@@ -1,20 +1,26 @@
 import { reportBenchmarkResults } from "../reportBenchmarkResults";
 import { strict as assert } from "assert";
 import fs from "fs";
+import path from "path";
 import "dotenv/config";
 
 async function main() {
-  console.log("Reporting tech support benchmark results");
+  const pathOut = path.join("testData", "tech_support_results.json");
+
+  console.log(`Reporting tech support benchmark results to ${pathOut}`);
+
   const apiKey = process.env.BRAINTRUST_API_KEY;
   assert(apiKey, "must have BRAINTRUST_API_KEY set");
+
   const projectName = "tech-support-prompt-completion";
+
   const cases = await reportBenchmarkResults({
     apiKey,
     projectName,
     experimentType: "prompt_response",
   });
   console.log(`Reported ${cases.length} cases`);
-  fs.writeFileSync("tech_support_results.json", JSON.stringify(cases, null, 2));
+  fs.writeFileSync(pathOut, JSON.stringify(cases, null, 2));
 }
 
 main();

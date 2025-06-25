d6d8d37cf42ca3dae55915750ffcc0da8f3053ab
ASteinheiser <me@iamandrew.io>
2025-06-25 14:11:17 -0700

    update comment

--- packages/mongodb-chatbot-server/src/routes/responses/responsesRouter.ts
+++ packages/mongodb-chatbot-server/src/routes/responses/responsesRouter.ts
@@ -22,6 +22,7 @@ export function makeResponsesRouter({ createResponse }: ResponsesRouterParams) {
 
   // TODO: add rate limit config
 
+  // Create Response API
   responsesRouter.post(
     "/",
     validateRequestSchema(CreateResponseRequest),

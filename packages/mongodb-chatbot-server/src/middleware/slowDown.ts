0399d3bb37d57838745b589e6a5df30eb053a590
Ben Perlmutter <mongodben@mongodb.com>
2025-06-25 10:42:20 -0400

    explanatory comments

--- packages/mongodb-chatbot-server/src/middleware/slowDown.ts
+++ packages/mongodb-chatbot-server/src/middleware/slowDown.ts
@@ -4,9 +4,9 @@ import { ipAddressKeyGenerator } from "./ipAddressKeyGenerator";
 export type SlowDownOptions = Partial<Options>;
 
 export const defaultSlowDownOptions = {
-  windowMs: 60 * 1000,
-  delayAfter: 20,
-  delayMs: 500,
+  windowMs: 60 * 1000, // 1 minute
+  delayAfter: 20, // allow 20 requests per windowMs before starting to delay
+  delayMs: 500, // delay 500ms for each request after the 20th request
   keyGenerator: ipAddressKeyGenerator,
 } satisfies SlowDownOptions;
 

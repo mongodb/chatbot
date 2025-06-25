9fc106e0d4c4b715a3923fd8edf9a022f3e9e8e5
ASteinheiser <me@iamandrew.io>
2025-06-24 19:02:16 -0700

    basic test for responses router, will be expanded once middleware is added

---
+++ packages/mongodb-chatbot-server/src/routes/responses/responsesRouter.test.ts
@@ -0,0 +1,40 @@
+import request from "supertest";
+import { AppConfig } from "../../app";
+import { DEFAULT_API_PREFIX } from "../../app";
+import { makeTestApp } from "../../test/testHelpers";
+import { makeTestAppConfig } from "../../test/testHelpers";
+
+jest.setTimeout(60000);
+
+describe("Responses Router", () => {
+  const ipAddress = "127.0.0.1";
+  const responsesEndpoint = DEFAULT_API_PREFIX + "/responses";
+  let appConfig: AppConfig;
+
+  beforeAll(async () => {
+    ({ appConfig } = await makeTestAppConfig());
+  });
+
+  it("should return 200 given a valid request", async () => {
+    const { app, origin } = await makeTestApp({
+      ...appConfig,
+      responsesRouterConfig: {
+        responses: {
+          createResponse: () => null,
+        },
+      },
+    });
+
+    const res = await request(app)
+      .post(responsesEndpoint)
+      .set("X-FORWARDED-FOR", ipAddress)
+      .set("Origin", origin)
+      .send({
+        model: "mongodb-chat-latest",
+        stream: true,
+        input: "What is MongoDB?",
+      });
+
+    expect(res.status).toBe(200);
+  });
+});

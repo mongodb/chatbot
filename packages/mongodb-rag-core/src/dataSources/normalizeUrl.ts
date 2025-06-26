bb316885e0e61c7a4a35ebdbf5d0d328f08e5ff6
Helen Schawe <helen.schawe@mongodb.com>
2025-06-26 14:18:34 -0500

    A bit safer normalization

--- packages/mongodb-rag-core/src/dataSources/normalizeUrl.ts
+++ packages/mongodb-rag-core/src/dataSources/normalizeUrl.ts
@@ -1,11 +1,11 @@
 /**
   Utility function that normalizes a URL.
-  Removes http/s protocol, www, trailing backslash.
+  Removes http/s protocol, www, trailing backslashes.
   DOES NOT remove query string and anchor tag.
 */
 export function normalizeUrl(url: string): string {
   return url
-    .replace(/^https?:\/\//, "")
+    .replace(/^https?:\/\//i, "")
     .replace(/^www\./, "")
-    .replace(/\/$/, "");
+    .replace(/\/+$/, "");
 }

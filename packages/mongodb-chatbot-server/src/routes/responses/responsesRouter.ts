7c918987842c43460033276a95085c9b39432038
ASteinheiser <me@iamandrew.io>
2025-06-26 17:35:52 -0700

    add rate limit and global slowdown to responses router

--- packages/mongodb-chatbot-server/src/routes/responses/responsesRouter.ts
+++ packages/mongodb-chatbot-server/src/routes/responses/responsesRouter.ts
@@ -1,8 +1,19 @@
 import Router from "express-promise-router";
 import { makeCreateResponseRoute } from "./createResponse";
 import type { GenerateResponse } from "../../processors";
+import {
+  makeRateLimit,
+  makeSlowDown,
+  type RateLimitOptions,
+  type SlowDownOptions,
+} from "../../middleware";
+import { makeRateLimitError } from "./errors";
 
 export interface ResponsesRouterParams {
+  rateLimitConfig?: {
+    routerRateLimitConfig?: RateLimitOptions;
+    routerSlowDownConfig?: SlowDownOptions;
+  };
   createResponse: {
     generateResponse: GenerateResponse;
     supportedModels: string[];
@@ -13,10 +24,34 @@ export interface ResponsesRouterParams {
 /**
   Constructor function to make the /responses/* Express.js router.
  */
-export function makeResponsesRouter({ createResponse }: ResponsesRouterParams) {
+export function makeResponsesRouter({
+  rateLimitConfig,
+  createResponse,
+}: ResponsesRouterParams) {
   const responsesRouter = Router();
 
-  // TODO: add rate limit config
+  /*
+    Global rate limit the requests to the responsesRouter.
+   */
+  const rateLimitErrorMessage =
+    rateLimitConfig?.routerRateLimitConfig?.message ?? "Rate limit exceeded";
+
+  responsesRouter.use((req, res, next) => {
+    const rateLimit = makeRateLimit({
+      ...rateLimitConfig?.routerRateLimitConfig,
+      message: makeRateLimitError({
+        error: new Error(rateLimitErrorMessage),
+        headers: req.headers as Record<string, string>,
+      }),
+    });
+    rateLimit(req, res, next);
+  });
+  /*
+    Slow down the response to the responsesRouter after certain number
+    of requests in the time window.
+   */
+  const globalSlowDown = makeSlowDown(rateLimitConfig?.routerSlowDownConfig);
+  responsesRouter.use(globalSlowDown);
 
   // Create Response API
   responsesRouter.post("/", makeCreateResponseRoute(createResponse));

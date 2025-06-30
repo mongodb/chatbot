00d7e21b8343c36951b007eadca68f551ec86133
Helen Schawe <helen.schawe@mongodb.com>
2025-06-30 18:05:29 -0500

    zod schema validation on customData origin/code

--- packages/chatbot-server-mongodb-public/src/processors/formatUserMessageForGeneration.ts
+++ packages/chatbot-server-mongodb-public/src/processors/formatUserMessageForGeneration.ts
@@ -1,34 +1,52 @@
-import { updateFrontMatter, logger } from "mongodb-rag-core";
-import { OriginCode } from "mongodb-chatbot-server";
+import {
+  updateFrontMatter,
+  logger,
+  ConversationCustomData,
+} from "mongodb-rag-core";
+import { originCodes } from "mongodb-chatbot-server";
+import { z } from "zod";
+
+const RawCustomDataSchema = z.object({
+  origin: z.string().describe("Origin of the request"),
+  originCode: z
+    .enum(originCodes)
+    // .optional()
+    .default("OTHER")
+    .describe("Code representing the origin of the request"),
+});
 
 export function formatUserMessageForGeneration(
   userMessageText: string,
-  customData?: {
-    origin?: string;
-    originCode?: OriginCode;
-    [key: string]: unknown;
-  }
-) {
-  const frontMatter: Record<string, string> = {};
-  const { origin, originCode } = customData ?? {};
-  if (typeof origin !== "string" || !origin) {
+  customData: ConversationCustomData
+): string {
+  const result = RawCustomDataSchema.safeParse(customData);
+  if (!result.success) {
+    logger.warn(
+      `Invalid customData provided ${customData}`,
+      result.error.message
+    );
     return userMessageText;
   }
+
+  const frontMatter: Record<string, string> = {};
+  const parsedCustomData = result.data;
   try {
-    const url = new URL(origin);
+    const url = new URL(parsedCustomData.origin);
     if (
       url.hostname === "mongodb.com" ||
       url.hostname.endsWith(".mongodb.com")
     ) {
-      frontMatter.pageUrl = origin;
+      frontMatter.pageUrl = parsedCustomData.origin;
     }
   } catch (e) {
-    logger.warn(`Origin ${origin} malformed. Not using in front matter.`);
+    logger.warn(
+      `Origin ${parsedCustomData.origin} malformed. Not using as URL in front matter.`
+    );
   }
 
-  if (originCode === "VSCODE") {
+  if (parsedCustomData.originCode === "VSCODE") {
     frontMatter.client = "MongoDB VS Code plugin";
-  } else if (originCode === "GEMINI_CODE_ASSIST") {
+  } else if (parsedCustomData.originCode === "GEMINI_CODE_ASSIST") {
     frontMatter.client = "Gemini Code Assist";
   }
 

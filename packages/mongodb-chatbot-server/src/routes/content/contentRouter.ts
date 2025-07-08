baad5bd60334e11d0f7cbc44e3f73c9579c854b7
mmeigs <matt.meigs@mongodb.com>
2025-07-08 10:19:31 -0400

    Work on clarity of comments in contentRouter

--- packages/mongodb-chatbot-server/src/routes/content/contentRouter.ts
+++ packages/mongodb-chatbot-server/src/routes/content/contentRouter.ts
@@ -1,10 +1,39 @@
-import { Router } from "express";
+import { RequestHandler, Router } from "express";
 import { FindContentFunc, MongoDbSearchResultsStore } from "mongodb-rag-core";
 import validateRequestSchema from "../../middleware/validateRequestSchema";
 import {
   SearchContentRequest,
   makeSearchContentRoute,
 } from "./searchContent";
+import { ParamsDictionary } from "express-serve-static-core";
+
+/**
+  Middleware to put in front of all the routes in the contentRouter.
+  Useful for authentication, data validation, logging, etc.
+  It exposes the app's {@link ContentRouterLocals} via {@link Response.locals}
+  ([docs](https://expressjs.com/en/api.html#res.locals)).
+  You can use or modify `res.locals.customData` in your middleware, and this data
+  will be available to subsequent middleware and route handlers.
+ */
+export type SearchContentMiddleware = RequestHandler<
+  ParamsDictionary,
+  unknown,
+  unknown,
+  unknown,
+  ContentRouterLocals
+>;
+
+/**
+  Local variables provided by Express.js for single request-response cycle
+ 
+  Keeps track of data for authentication or dynamic data validation.
+ */
+export interface ContentRouterLocals {
+  customData: Record<string, unknown>;
+}
+
+
+
 
 export interface MakeContentRouterParams {
   findContent: FindContentFunc;

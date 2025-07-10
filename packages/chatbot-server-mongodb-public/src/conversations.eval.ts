eeaf5c21c473c2c5ae8997c7fb30449c1fedcdc8
Helen Schawe <helen.schawe@mongodb.com>
2025-07-10 12:26:58 -0500

    A couple more eval cases for system prompt adhereance, general response performance

--- packages/chatbot-server-mongodb-public/src/conversations.eval.ts
+++ packages/chatbot-server-mongodb-public/src/conversations.eval.ts
@@ -27,7 +27,7 @@ async function conversationEval() {
     )
   );
 
-  const conversationEvalCases = [...miscCases, ...faqCases, ...dotComCases];
+  const conversationEvalCases = [...miscCases].filter((e) => {return e.tags?.includes("fetch_page")}) //, ...faqCases, ...dotComCases];
 
   try {
     // Run the conversation eval

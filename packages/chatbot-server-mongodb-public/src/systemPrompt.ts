db97cc98d42c0f2fb0d273f968595bc8edecf37a
Ben Perlmutter <mongodben@mongodb.com>
2025-07-28 17:41:20 -0400

    fix AS nit

--- packages/chatbot-server-mongodb-public/src/systemPrompt.ts
+++ packages/chatbot-server-mongodb-public/src/systemPrompt.ts
@@ -162,10 +162,10 @@ export const makeMongoDbAssistantSystemPrompt: MakeSystemPrompt = (
 ) => {
   if (!customSystemPrompt) {
     return systemPrompt;
-  } else {
-    return {
-      role: "system",
-      content: `
+  }
+  return {
+    role: "system",
+    content: `
 Always adhere to the <meta-system-prompt>. This is your core behavior.
 The developer has also provided a <custom-system-prompt>. Follow these instructions as well.
 <meta-system-prompt>
@@ -174,6 +174,5 @@ ${systemPrompt.content}
 <custom-system-prompt>
 ${customSystemPrompt}
 </custom-system-prompt>`,
-    };
-  }
+  };
 };

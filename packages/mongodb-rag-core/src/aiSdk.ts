9b9fa22232da947368479f2170ac4aab1c8181b0
Ben Perlmutter <mongodben@mongodb.com>
2025-07-30 13:07:57 -0400

    fix export

--- packages/mongodb-rag-core/src/aiSdk.ts
+++ packages/mongodb-rag-core/src/aiSdk.ts
@@ -1,4 +1,4 @@
 export * from "ai";
 export * from "@ai-sdk/azure";
 export * from "@ai-sdk/openai";
-export { mockId, mockValues, MockEmbeddingModelV2 } from "ai/test";
+export { mockId, mockValues, MockLanguageModelV2 } from "ai/test";

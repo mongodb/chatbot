15def1ad2a37492930c956547a42ffa0e9f9cf08
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2025-08-06 10:15:46 -0400

    Update packages/mongodb-chatbot-server/src/routes/responses/createResponse.test.ts
    Co-authored-by: Nick Larew <nick.larew@mongodb.com>

--- packages/mongodb-chatbot-server/src/routes/responses/createResponse.test.ts
+++ packages/mongodb-chatbot-server/src/routes/responses/createResponse.test.ts
@@ -18,7 +18,6 @@ import {
   TEST_ALWAYS_ALLOWED_METADATA_KEYS,
   makeDefaultConfig,
   mockUpdateTrace,
-  // mockUpdateTrace,
 } from "../../test/testConfig";
 import {
   CREATE_RESPONSE_ERR_MSG,

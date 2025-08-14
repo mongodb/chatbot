f85445f3e90f883efa1c616eccc3643341c2f3e0
Nick Larew <nick.larew@mongodb.com>
2025-08-14 17:57:50 -0500

    (New package) Mercury case analysis (#876)
    * WIP - move to new package
    
    * move to new package
    
    * suggest rewrites
    
    * case analysis release ready
    
    * ready for publish
    
    * copilot review
    
    * be a module, use tsup
    
    * embedders -> embeddingModels
    
    * export rag-core types
    
    * BP review

---
+++ packages/mercury-case-analysis/src/index.ts
@@ -0,0 +1,6 @@
+export * from "./analyzeCases";
+export * from "./rating";
+export * from "./relevance";
+export * from "./suggestRewrite";
+export { EmbeddingModel, LanguageModel } from "mongodb-rag-core/aiSdk";
+export { BraintrustMiddleware } from "mongodb-rag-core/braintrust";

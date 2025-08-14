2047095be77c7055c255baabb23b18d4e790be59
Nick Larew <nick.larew@mongodb.com>
2025-08-14 17:07:38 -0500

    export rag-core types

--- packages/mercury-case-analysis/src/index.ts
+++ packages/mercury-case-analysis/src/index.ts
@@ -2,3 +2,5 @@ export * from "./analyzeCases";
 export * from "./rating";
 export * from "./relevance";
 export * from "./suggestRewrite";
+export { EmbeddingModel, LanguageModel } from "mongodb-rag-core/aiSdk";
+export { BraintrustMiddleware } from "mongodb-rag-core/braintrust";

bc9cb63cab9ab6c3ca469d56e886c07d69af0663
Nick Larew <nick.larew@mongodb.com>
2025-08-27 12:02:32 -0500

    Models to shared folder

--- packages/scripts/src/mercury/models.ts
+++ packages/scripts/src/mercury/models.ts
@@ -28,3 +28,41 @@ export function getModelDeployment(label: string) {
     throw new Error(`Cannot get deployment for ${label} - model not found`);
   }
 }
+
+export const mercuryModelConfigs = getModels([
+  // OpenAI
+  "gpt-5",
+  "gpt-5-mini",
+  "gpt-5-nano",
+  "gpt-4o",
+  "gpt-4o-mini",
+  "o3",
+  "o3-mini",
+  "o4-mini",
+  "gpt-4.1",
+  "gpt-4.1-mini",
+  "gpt-4.1-nano",
+  // Anthropic
+  "claude-opus-4",
+  "claude-sonnet-4",
+  "claude-37-sonnet",
+  "claude-35-sonnet-v2",
+  "claude-35-sonnet",
+  "claude-35-haiku",
+  // Meta
+  "llama-3.1-70b",
+  "llama-3.2-90b",
+  "llama-3.3-70b",
+  // Google
+  "gemini-2.0-flash-lite-001",
+  "gemini-2-flash",
+  "gemini-2.5-flash-lite",
+  "gemini-2.5-flash",
+  "gemini-2.5-pro",
+  // Amazon
+  "nova-micro-v1:0",
+  "nova-lite-v1:0",
+  "nova-pro-v1:0",
+  // Mistral
+  "mistral-large-2",
+]);

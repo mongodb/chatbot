7b43359839967072f1886b12dbcfda412fa8c05d
Ben Perlmutter <mongodben@mongodb.com>
2025-08-28 16:20:18 -0400

    test resiliency

--- packages/ingest-mongodb-public/src/sources/snootySources.test.ts
+++ packages/ingest-mongodb-public/src/sources/snootySources.test.ts
@@ -6,6 +6,7 @@ import {
 
 jest.setTimeout(20000);
 describe("Snooty data sources", () => {
+  jest.retryTimes(3, { waitBeforeRetry: 5000, logErrorsBeforeRetry: true });
   test.each(snootyProjectConfig)("$name should fetch data", async (project) => {
     const [source] = await makeSnootyDataSources(snootyDataApiBaseUrl, [
       project,
